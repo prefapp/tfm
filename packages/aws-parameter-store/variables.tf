58c200b52fe78d021ee882fb7634653649095d21
Antonio R. Vega <arvegadev@hotmail.com>
2022-08-23 14:35:16 +0000

    Add tf code

---
+++ packages/aws-parameter-store/variables.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  default     = "eu-west-1"
+  description = "AWS region"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+}
+
+variable "aws_account" {
+  description = "AWS Account ID"
+  type        = string
+}
+
+variable "services" {
+  type = map(object({
+    name = string
+  }))
+
+  default = {}
+
+}
+
+variable "env" {
+  description = "Environment"
+  type        = string
+}
