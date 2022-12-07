63e2575a5ce39acaa72b5f9426614cb3a74ca2e5
Antonio R. Vega <arvegadev@hotmail.com>
2022-12-07 17:34:30 +0000

    Fix user-groups creation

--- modules/aws-sso/main.tf
+++ modules/aws-sso/main.tf
@@ -5,7 +5,7 @@ locals {
 
   users_by_name = {
 
-    for user in local.yaml.users : user.name => user   
+    for user in local.yaml.users : user.name => user
 
   }
 
@@ -19,7 +19,7 @@ locals {
 
     for group in local.yaml.groups : [
 
-      for user in group.users: "${user},${group.name}"
+      for user in group.users : "${user},${group.name}"
 
     ]
 
@@ -27,30 +27,38 @@ locals {
 
 }
 
-# We need to create the users 
+# We need to create the users
 resource "aws_identitystore_user" "users" {
-  
-  for_each = { local.users_by_name }
+
+  for_each = local.users_by_name
 
   identity_store_id = var.identity_store_arn
 
   user_name = each.value.email
- 
-  name = each.value.fullname ? each.value.fullname : each.key
+
+  display_name = each.value.email
+
+  name {
+
+    given_name = each.value.fullname != null ? each.value.fullname : each.key
+
+    family_name = each.value.fullname != null ? each.value.fullname : each.key
+
+  }
 
   emails {
 
-     value = each.value.email
+    value = each.value.email
 
-  }  
+  }
 
 }
 
 resource "aws_identitystore_group" "groups" {
 
-  for_each = { local.groups_by_name }
-  
-  display_name = each.name
+  for_each = local.groups_by_name
+
+  display_name = each.value.name
 
   identity_store_id = var.identity_store_arn
 
@@ -62,14 +70,21 @@ resource "aws_identitystore_group" "groups" {
 }
 
 resource "aws_identitystore_group_membership" "groups-users" {
-  
+
+  for_each = toset(local.users_in_groups)
+
   identity_store_id = var.identity_store_arn
 
-  for_each = { local.users_in_groups }
+  group_id = aws_identitystore_group.groups[split(",", each.value)[1]].group_id
 
-  group_id          = aws_identitystore_group.groups[split(",", each.key)[1]].group_id
+  member_id = aws_identitystore_user.users[split(",", each.value)[0]].user_id
 
-  member_id         = aws_identitystore_user.users[split(",", each.key)[0]].user_id
+  depends_on = [
+
+    aws_identitystore_group.groups,
+
+    aws_identitystore_user.users
+
+  ]
 
 }
-
