a33bcf1802348c4de51df3edebd1965b5a0f2f93
frmadem <frmadem@gmail.com>
2023-06-07 13:05:13 +0200

    Add new machinery (#19)
    * Add new machinery
    
    * Fix problems on dns
    
    * Dns control
    
    * Add azure.extensions
    
    * Add point in time restore mode
    
    * Add flexible pitr restore
    
    * Add tags
    
    * Add tags for virtual network
    
    * Add ids as parameters
    
    * Add id selector
    
    * Avoid a fix zone
    
    * Add replication role
    
    * Ignore lifecycle
    
    * Fix typo
    
    * Add Readme
    
    ---------
    
    Co-authored-by: frmadem <francisco.maseda@prefapp.es>

--- modules/azure-flexible-server-postgresql/README.md
+++ modules/azure-flexible-server-postgresql/README.md
@@ -1,64 +1,129 @@
 # Terraform Module: azurerm_postgresql_flexible_server
 
-This Terraform module creates a PostgreSQL Flexible Server on Azure with the following features:
+This Terraform Module creates a [Azure PostgreSQL Flexible Server](https://learn.microsoft.com/en-us/azure/postgresql/flexible-server/) with the following features:
 
-- Creates a single Flexible Server and a Resource Group
-- Configurable backup retention days (7-35)
-
-## Requirements
-
-- AzureRM Provider version 3.55.0 or later.
-- Access to a virtual network and subnet.
-- An Azure Key Vault secret with the PostgreSQL administrator password.
+- It creates a single Flexible Server
+- It can create it from a PITr
+- It updates the admin password or takes if from an Azure KeyVault Secret
+- It can update and manage Server additional configurations
 
 ## Usage
 
-To use the module, include the following code in your Terraform configuration:
+Module usage:
 
 ```hcl
+
 module "postgresql" {
-  source = "git::https://github.com/prefapp/tfm.git//modules/azure-flexible-server-postgresql?ref=azure-flexible-server-postgresql-v1.0.0"
-  env                                  = "dev"
-  cliente                              = "acme"
-  producto                             = "my-product"
-  location                             = "eastus"
-  virtual_network_name                 = "my-virtual-network"
-  virtual_network_rg                   = "my-virtual-network-rg"
-  virtual_network_subnet_name          = "my-subnet"
-  postgresql_name                      = "my-postgresql"
-  postgresql_version                   = "14"
-  backup_retention_days                = "7"
-  authentication_password_auth_enabled = "true"
-  administrator_login                  = "admin"
-  administrator_password_key_vault     = "my-key-vault"
-  administrator_password_secret_name   = "my-postgresql-secret"
-  postgresql_disk_size                 = "5120"
-  postgresql_sku_size                  = "Standard_D4s_v3"
+
+ source = "<module url>"
+
+ location = "westeurope"
+
+ data = yamldecode(file("<path to yaml file with the data>"))
 }
+
 ```
 
-|Name|Description|Type|Default|Required|
-|----|-----------|----|-------|--------|
-|env|The environment to deploy the resources into.|string|n/a|yes|
-|cliente|The name of the client or organization.|string|n/a|yes|
-|producto|The name of the product or application.|string|n/a|yes|
-|location|The Azure region where resources will be created.|string|n/a|yes|
-|virtual_network_name|The name of the existing virtual network.|string|n/a|yes|
-|virtual_network_rg|The name of the existing resource group containing the virtual network.|string|n/a|yes|
-|virtual_network_subnet_name|The name of the existing subnet where the server should be deployed.|string|n/a|yes|
-|postgresql_name|The name of the PostgreSQL server to create.|string|n/a|yes|
-|postgresql_version|The version of PostgreSQL to deploy.|string|"13|no|
-|backup_retention_days|The number of days to retain backups for. Valid values are between 7 and 35.|string|"7"|no|
-|authentication_active_directory_auth_enabled|Enable or disable Azure Active Directory authentication.|bool|"false"|no|
-|authentication_password_auth_enabled|Enable or disable password authentication.|bool|"true"|no|
-|administrator_login|The administrator login for the PostgreSQL server.|string|n/a|yes|
-|administrator_password_key_vault|The name of the Key Vault containing the PostgreSQL administrator password.|string|n/a|yes|
-|administrator_password_secret_name|The name of the secret containing the PostgreSQL administrator password.|string|n/a|yes|
-|postgresql_disk_size|The size of disk in MiB|string|n/a|yes|
-|postgresql_sku_size|The SKU name|string|n/a|yes|
-|dns_private_zone_dns_name|The name of the Private DNS zone (without a terminating dot)|string|n/a|yes|
-|dns_private_zone_dns_rg|Specifies the resource group where the Private DNS Zone exists.|string|n/a|yes|
 
-## Note
+## Requirements
 
-Change de ref into source module `ref=1.0.0`
+### Inputs
+
+- A DNS private zone
+- A resource group
+- A subnet with a Service Delegation for ```Microsoft.DBforPostgreSQL/flexibleServers``` and service endpoints to ```Microsoft.Storage```
+- A keyvault to store/read a secret with the PostgreSQL admin pass
+
+### Yaml input
+
+The module can be used with data expressed in a yaml file such as:
+
+```yaml
+
+server:
+  name: mi-server
+  version: 12
+  disk_size: 32768
+  sku_name: "GP_Standard_D2ds_v4"
+  tags: {}
+  zone: "1"
+  replication_role:
+
+server_creation:
+  mode: Default
+  #mode: PointInTimeRestore
+  #from_pitr:
+  #  source_server_name: "mi-original-server"
+  #  source_server_resource_group: "mi-original-server-rg"
+  #  pitr: "2018-03-13T13:59:00Z"
+
+
+server_parameters:
+  azure_extensions:
+    - PG_BUFFERCACHE
+    - PG_STAT_STATEMENTS
+    - PLPGSQL
+    - UNACCENT
+    - UUID-OSSP
+
+backup_retention_days: 12
+
+administrator_login: pgadmin
+
+maintainance_window:
+  day_of_week: 6
+  start_hour: 0
+  start_minute: 0
+
+authentication:
+  password_auth_enabled: true
+  active_directory_auth_enabled: false
+
+password:
+  key_vault_name: test-tfm-prefapp2
+  key_vault_resource_group: test-modulo
+  key_vault_secret_name: pg-pass2
+  create: true
+
+resource_group:
+  name: test-modulo
+
+
+subnet:
+  id: "/subscriptions/152234234/resourceGroups/test-modulo/providers/Microsoft.Network/virtualNetworks/test-modulo/subnets/default"
+  #name: default
+  vnet:
+    name: test-modulo
+    resource_group: test-modulo
+
+dns:
+  private:
+    name: test.tfm.postgres.database.azure.com
+    resource_group: test-modulo
+
+```
+
+## Optional Password creation
+
+Now it suffises to pass a key vault , a resource group and a key name to have a random password created, inserted as a secret in the KV and as the pass of the server. 
+
+```yaml
+password:
+  key_vault_name: test-tfm-prefapp
+  key_vault_resource_group: test-modulo
+  key_vault_secret_name: pg-pass
+  create: true
+```
+
+If the password is to be supplied not created by the module:
+
+```yaml
+password:
+  key_vault_name: test-tfm-prefapp
+  key_vault_resource_group: test-modulo
+  key_vault_secret_name: pg-pass
+  create: false
+```
+
+
+
--- modules/azure-flexible-server-postgresql/main.tf
+++ modules/azure-flexible-server-postgresql/main.tf
@@ -11,64 +11,193 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "resource_group" {
-  name     = "fs-psql-${var.env}-${var.postgresql_name}"
-  location = var.location
-  tags = {
-    env      = var.env
-    cliente  = var.cliente
-    producto = var.producto
+locals {
+
+  # let's read the data first
+  data = var.data
+
+  # ok let's calculate the subnet_id
+  subnet_id = lookup(local.data.subnet, "name", "NOT_DEFINED")  == "NOT_DEFINED" ? local.data.subnet.id : data.azurerm_subnet.subnet[0].id
+}
+
+#---------------------------------------------------------
+#
+# Password creation / control
+#
+#---------------------------------------------------------
+
+## We only generate the password if necessary
+resource "random_password" "password" {
+  length  = 20
+  special = true
+}
+
+
+## We only upload the password if necessary
+## Once created we ignore it
+resource "azurerm_key_vault_secret" "password_create" {
+  count        = local.data.password.create ? 1 : 0
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+  name         = local.data.password.key_vault_secret_name
+  value        = random_password.password.result
+  lifecycle {
+
+    ignore_changes = [value]
   }
 }
 
-data "azurerm_virtual_network" "virtual_network" {
-  name                = var.virtual_network_name
-  resource_group_name = var.virtual_network_rg
-}
-
+#
+# Checkers and data access
+#
 data "azurerm_key_vault" "key_vault" {
-  name                = var.password_key_vault
-  resource_group_name = var.password_key_vault_rg
+  name                = local.data.password.key_vault_name
+  resource_group_name = local.data.password.key_vault_resource_group
 }
 
 data "azurerm_key_vault_secret" "password" {
-  name         = var.password_secret_name
+  name         = local.data.password.key_vault_secret_name
   key_vault_id = data.azurerm_key_vault.key_vault.id
+
+  depends_on = [
+
+    azurerm_key_vault_secret.password_create
+
+  ]
 }
 
+
+#---------------------------------------------------------
+#
+# Network management
+#
+#---------------------------------------------------------
+data "azurerm_subnet" "subnet" {
+
+  count = lookup(local.data.subnet, "name", "NOT_DEFINED") == "NOT_DEFINED" ? 0 : 1
+
+  name = local.data.subnet.name
+
+  virtual_network_name = local.data.subnet.vnet.name
+
+  resource_group_name = local.data.subnet.vnet.resource_group
+
+}
+
+#---------------------------------------------------------
+#
+# DNS management
+#
+#---------------------------------------------------------
 data "azurerm_private_dns_zone" "private_dns_zone" {
-  name                = var.dns_private_zone_dns_name
-  resource_group_name = var.dns_private_zone_dns_rg
+
+  name = local.data.dns.private.name
+
+  resource_group_name = local.data.dns.private.resource_group
+
+}
+
+
+#---------------------------------------------------------
+#
+# Server management
+#
+#---------------------------------------------------------
+
+data "azurerm_resource_group" "resource_group" {
+
+  name = local.data.resource_group.name
+
+}
+
+data "azurerm_postgresql_flexible_server" "postgresql_restore_original_server" {
+
+  count = local.data.server_creation.mode == "PointInTimeRestore" ? 1 : 0
+
+  name = local.data.server_creation.from_pitr.source_server_name
+
+  resource_group_name = local.data.server_creation.from_pitr.source_server_resource_group
+
 }
 
 resource "azurerm_postgresql_flexible_server" "postgresql_flexible_server" {
-  name                  = "${var.env}-${var.postgresql_name}"
-  resource_group_name   = azurerm_resource_group.resource_group.name
-  location              = var.location
-  version               = var.postgresql_version
-  delegated_subnet_id   = "${data.azurerm_virtual_network.virtual_network.id}/subnets/${var.virtual_network_subnet_name}"
-  private_dns_zone_id   = data.azurerm_private_dns_zone.private_dns_zone.id
-  backup_retention_days = var.backup_retention_days
+
+  name = local.data.server.name
+
+  resource_group_name = local.data.resource_group.name
+
+  location = var.location
+
+  version = local.data.server.version
+
+  delegated_subnet_id = local.subnet_id
+
+  private_dns_zone_id = data.azurerm_private_dns_zone.private_dns_zone.id
+
+  backup_retention_days = local.data.backup_retention_days
+
+  create_mode = local.data.server_creation.mode
+
+  point_in_time_restore_time_in_utc = local.data.server_creation.mode == "PointInTimeRestore" ? local.data.server_creation.from_pitr.pitr : null
+
+  source_server_id = local.data.server_creation.mode == "PointInTimeRestore" ? data.azurerm_postgresql_flexible_server.postgresql_restore_original_server[0].id : null
+
   authentication {
-    active_directory_auth_enabled = var.authentication_active_directory_auth_enabled
-    password_auth_enabled         = var.authentication_password_auth_enabled
+
+    active_directory_auth_enabled = local.data.authentication.active_directory_auth_enabled
+
+    password_auth_enabled = local.data.authentication.password_auth_enabled
+
   }
+
   maintenance_window {
-    day_of_week  = var.postgresql_maintenance_window_day_of_week
-    start_hour   = var.postgresql_maintenance_window_start_hour
-    start_minute = var.postgresql_maintenance_window_start_minute
+
+    day_of_week = local.data.maintainance_window.day_of_week
+
+    start_hour = local.data.maintainance_window.start_hour
+
+    start_minute = local.data.maintainance_window.start_minute
+
   }
-  administrator_login    = var.administrator_login
+
+  administrator_login = local.data.administrator_login
+
   administrator_password = data.azurerm_key_vault_secret.password.value
-  zone                   = "1"
-  storage_mb             = var.postgresql_disk_size
-  sku_name               = var.postgresql_sku_size
-  tags = {
-    env      = var.env
-    cliente  = var.cliente
-    producto = var.producto
-  }
+
+  zone = lookup(local.data.server, "zone", null)
+
+  storage_mb = local.data.server.disk_size
+
+  sku_name = local.data.server.sku_name
+
+  replication_role = lookup(local.data.server, "replication_role", "None")
+
+  tags = local.data.server.tags
+
   depends_on = [
-    azurerm_resource_group.resource_group
+
+    azurerm_key_vault_secret.password_create
+
   ]
+
+  lifecycle {
+
+    ignore_changes = [
+
+      version
+
+    ]
+
+  }
+}
+
+resource "azurerm_postgresql_flexible_server_configuration" "postgresql_flexible_server_conf" {
+
+  count = length(local.data.server_parameters.azure_extensions) > 0 ? 1 : 0
+
+  name = "azure.extensions"
+
+  server_id = azurerm_postgresql_flexible_server.postgresql_flexible_server.id
+
+  value = join(",", local.data.server_parameters.azure_extensions)
+
 }
