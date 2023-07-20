6d7f9d94874960ec3afcac7d45fab8249b0a7c33
Juan CFF <97174951+jcframil@users.noreply.github.com>
2023-07-20 11:03:08 +0200

    Update main.tf
    Remove provider configuration for a reusable module

--- modules/azure-flexible-server-postgresql/main.tf
+++ modules/azure-flexible-server-postgresql/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.55.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 locals {
   # Let's read the data first
   data = var.data
