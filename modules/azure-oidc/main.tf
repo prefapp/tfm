82385c1d74d6cfd812e09573c032d5f79ba09eb9
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2023-08-29 12:01:48 +0200

    Test

--- modules/azure-oidc/main.tf
+++ modules/azure-oidc/main.tf
@@ -21,28 +21,30 @@ data "azuread_client_config" "current" {}
 
 data "azurerm_subscription" "primary" {}
 
-
 resource "azuread_application" "gh_oidc_ad_app" {
-  display_name = "GitHub OIDC Management - ${var.application}"
-  owners = [data.azuread_client_config.current.object_id]
+  for_each     = { for app in var.data.applications : app.name => app }
+  display_name = "GitHub OIDC Management - ${each.value.name}"
+  owners       = [data.azuread_client_config.current.object_id]
 }
 
 resource "azuread_service_principal" "gh_oidc_service_principal" {
-  application_id = azuread_application.gh_oidc_ad_app.application_id
+  for_each     = azuread_application.gh_oidc_ad_app
+  application_id = each.value.application_id
 }
 
 resource "azurerm_role_assignment" "gh_oidc_service_role_assignment" {
-  scope = join("", [data.azurerm_subscription.primary.id, "/resourceGroups/cbx-acr/providers/Microsoft.ContainerRegistry/registries/cbxacr"])
+  for_each             = { for app in var.data.applications : app.name => app }
+  scope                = join("", [data.azurerm_subscription.primary.id, each.value.scopes[0]])
   role_definition_name = "AcrPull"
-  principal_id         = azuread_service_principal.gh_oidc_service_principal.object_id
+  principal_id         = azuread_service_principal.gh_oidc_service_principal[each.key].object_id
 }
 
 resource "azuread_application_federated_identity_credential" "gh_oidc_identity_credential" {
-  for_each              = toset(var.subs)
-  application_object_id = azuread_application.gh_oidc_ad_app.object_id
+  for_each              = var.data.applications
+  application_object_id = azuread_application.gh_oidc_ad_app[each.value.name].object_id
   display_name          = "gh_oidc_identity_credential"
   description           = "Github OIDC Identity Credential"
   audiences             = ["api://AzureADTokenExchange"]
   issuer                = "https://token.actions.githubusercontent.com"
-  subject               = "${each.key}"
+  subject               = each.value.name
 }
