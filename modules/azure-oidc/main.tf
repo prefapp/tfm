3ee205abda0f915ca643dae71acacd18c453a3f5
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2023-08-29 15:17:31 +0200

    Refactor azure-oidc module

--- modules/azure-oidc/main.tf
+++ modules/azure-oidc/main.tf
@@ -21,28 +21,42 @@ data "azuread_client_config" "current" {}
 
 data "azurerm_subscription" "primary" {}
 
-
 resource "azuread_application" "gh_oidc_ad_app" {
-  display_name = "GitHub OIDC Management"
-  owners = [data.azuread_client_config.current.object_id]
+  for_each     = { for app in var.data.applications : app.name => app }
+  display_name = "GitHub OIDC Management - ${each.value.name}"
+  owners       = [data.azuread_client_config.current.object_id]
 }
 
 resource "azuread_service_principal" "gh_oidc_service_principal" {
-  application_id = azuread_application.gh_oidc_ad_app.application_id
-}
-
-resource "azurerm_role_assignment" "gh_oidc_service_role_assignment" {
-  scope                = data.azurerm_subscription.primary.id
-  role_definition_name = "${var.role_definition_name}"
-  principal_id         = azuread_service_principal.gh_oidc_service_principal.object_id
+  for_each     = azuread_application.gh_oidc_ad_app
+  application_id = each.value.application_id
 }
 
 resource "azuread_application_federated_identity_credential" "gh_oidc_identity_credential" {
-  for_each              = toset(var.subs)
-  application_object_id = azuread_application.gh_oidc_ad_app.object_id
+  for_each              = { for app in var.data.applications : app.name => app }
+  application_object_id = azuread_application.gh_oidc_ad_app[each.value.name].object_id
   display_name          = "gh_oidc_identity_credential"
   description           = "Github OIDC Identity Credential"
   audiences             = ["api://AzureADTokenExchange"]
   issuer                = "https://token.actions.githubusercontent.com"
-  subject               = "${each.key}"
+  subject               = each.value.name
+}
+
+resource "azurerm_role_assignment" "gh_oidc_service_role_assignment" {
+  for_each             = { 
+    for item in flatten ([
+      for app in var.data.applications : [
+        for role in app.roles : [
+          for scope in lookup(app, "scope", [data.azurerm_subscription.primary.id]) : {
+            app_name = app.name
+            role_name = role
+            app_scope = scope
+          }
+        ]
+      ]
+    ]) : format("%s-%s-%s", item.app_name, item.role_name, item.app_scope) => item
+  }
+  scope                = each.value.app_scope
+  role_definition_name = each.value.role_name
+  principal_id         = azuread_service_principal.gh_oidc_service_principal[each.value.app_name].id
 }
