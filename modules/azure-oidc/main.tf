64ab4b2f479662f9cb96e648033d55b988b51865
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2023-08-29 11:46:47 +0200

    Test scope

--- modules/azure-oidc/main.tf
+++ modules/azure-oidc/main.tf
@@ -33,7 +33,7 @@ resource "azuread_service_principal" "gh_oidc_service_principal" {
 
 resource "azurerm_role_assignment" "gh_oidc_service_role_assignment" {
   # scope is data.azurerm_subscription.primary.id + "/resourceGroups/foo"
-  scope                = data.azurerm_subscription.primary.id + "/resourceGroups/foo"
+  scope = join("/", [data.azurerm_subscription.primary.id, "resourceGroups/foo"])
   role_definition_name = "${var.role_definition_name}"
   principal_id         = azuread_service_principal.gh_oidc_service_principal.object_id
 }
