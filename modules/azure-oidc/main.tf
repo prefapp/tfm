b8980f2c058c55676b04ec3e3479d1823d41dc03
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2023-08-29 14:19:00 +0200

    Test

--- modules/azure-oidc/main.tf
+++ modules/azure-oidc/main.tf
@@ -64,7 +64,7 @@ resource "azurerm_role_assignment" "gh_oidc_service_role_assignment" {
       ]
     ]) : format("%s-%s", item.app_name, item.role_name) => item
   }
-  scope                = lookup(each.value, "scope", data.azurerm_subscription.primary.id)
+  scope                = lookup(each.value, "app_scope", data.azurerm_subscription.primary.id)
   role_definition_name = each.value.role_name
   principal_id         = azuread_service_principal.gh_oidc_service_principal[each.value.app_name].id
 }
