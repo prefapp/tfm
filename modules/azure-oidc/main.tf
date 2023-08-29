23d9dff08e87883a7f2aadb65c072ad60c1f6b1c
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2023-08-29 13:26:05 +0200

    Test

--- modules/azure-oidc/main.tf
+++ modules/azure-oidc/main.tf
@@ -60,9 +60,16 @@ resource "azuread_application_federated_identity_credential" "gh_oidc_identity_c
 
 
 resource "azurerm_role_assignment" "gh_oidc_service_role_assignment" {
-  # for each app and for each role in the app, create a role assignment
-  for_each             = { for app in var.data.applications : app.name => app }
+  for_each = {
+    for app in var.data.applications :
+    "${app.name}-${role}" => {
+      app_name         = app.name
+      role_definition = role
+    }
+    for role in app.roles
+  }
+
   scope                = "/subscriptions/0ded1d7a-f274-44db-8e97-d56340081450/resourceGroups/cbx-acr/providers/Microsoft.ContainerRegistry/registries/cbxacr"
-  role_definition_name = { for role in each.value.roles : role => role }
-  principal_id         = azuread_service_principal.gh_oidc_service_principal[each.key].object_id
+  role_definition_name = each.value.role_definition
+  principal_id         = azuread_service_principal.gh_oidc_service_principal[each.value.app_name].object_id
 }
