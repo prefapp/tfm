dfe39819d120b9db58e1a36891a90d9de66ca7b3
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2023-08-30 11:37:06 +0200

    Clean main file

--- modules/azure-oidc/main.tf
+++ modules/azure-oidc/main.tf
@@ -32,65 +32,6 @@ resource "azuread_service_principal" "gh_oidc_service_principal" {
   application_id = each.value.application_id
 }
 
-# app_registrations:
-#   - name: service_repositories
-#     roles:
-#       - AcrPush
-#       - AcrPull
-#     scope:
-#       - "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/foo/providers/Microsoft.ContainerRegistry/registries/foo-registries"
-#       - "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/bar/providers/Microsoft.ContainerRegistry/registries/bar-registries"
-#     federated_credentials:
-#       - subject: "repository_owner:prefapp"
-#         issuer: "https://token.actions.githubusercontent.com"
-#   - name: state_repositorie
-#     roles:
-#       - AcrPush
-#     scope:
-#       - "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/foo/providers/Microsoft.ContainerRegistry/registries/foo-registries"
-#     federated_credentials:
-#       - subject: "repository_owner:prefapp"
-#         issuer: "https://token.actions.githubusercontent.com"
-#   - name: infra_repositorie
-#     roles:
-#       - Contributor
-#     federated_credentials:
-#       - subject: "my_repo_foo"
-#         issuer: "issuer_foo"
-#       - subject: "my_repo_bar"
-#         issuer: "issuer_bar"
-#     # scope:
-#     #   - "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" # This is similar to not putting scope
-
-resource "azurerm_role_assignment" "gh_oidc_service_role_assignment" {
-  for_each             = { 
-    for item in flatten ([
-      for app in var.data.applications : [
-        for role in app.roles : [
-          for scope in lookup(app, "scope", [data.azurerm_subscription.primary.id]) : {
-            app_name = app.name
-            role_name = role
-            app_scope = scope
-          }
-        ]
-      ]
-    ]) : format("%s-%s-%s", item.app_name, item.role_name, item.app_scope) => item
-  }
-  scope                = each.value.app_scope
-  role_definition_name = each.value.role_name
-  principal_id         = azuread_service_principal.gh_oidc_service_principal[each.value.app_name].id
-}
-
-# resource "azuread_application_federated_identity_credential" "gh_oidc_identity_credential" {
-#   for_each              = { for app in var.data.applications : app.name => app }
-#   application_object_id = azuread_application.gh_oidc_ad_app[each.value.name].object_id
-#   display_name          = "oidc_identity_credential"
-#   description           = var.data.subject
-#   audiences             = ["api://AzureADTokenExchange"]
-#   issuer                = "https://token.actions.githubusercontent.com"
-#   subject               = var.data.subject
-# }
-
 resource "azuread_application_federated_identity_credential" "gh_oidc_identity_credential" {
   for_each = {
     for item in flatten ([
@@ -109,4 +50,23 @@ resource "azuread_application_federated_identity_credential" "gh_oidc_identity_c
   audiences             = ["api://AzureADTokenExchange"]
   issuer                = each.value.cred.issuer
   subject               = each.value.cred.subject
+}
+
+resource "azurerm_role_assignment" "gh_oidc_service_role_assignment" {
+  for_each             = { 
+    for item in flatten ([
+      for app in var.data.applications : [
+        for role in app.roles : [
+          for scope in lookup(app, "scope", [data.azurerm_subscription.primary.id]) : {
+            app_name = app.name
+            role_name = role
+            app_scope = scope
+          }
+        ]
+      ]
+    ]) : format("%s-%s-%s", item.app_name, item.role_name, item.app_scope) => item
+  }
+  scope                = each.value.app_scope
+  role_definition_name = each.value.role_name
+  principal_id         = azuread_service_principal.gh_oidc_service_principal[each.value.app_name].id
 }
