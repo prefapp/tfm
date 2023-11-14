bb9cab371f31c1e612b40f383494b38ffe986342
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2023-11-14 10:57:39 +0100

    Test

--- modules/azure-resource-groups/resource_groups.tf
+++ modules/azure-resource-groups/resource_groups.tf
@@ -1,6 +1,6 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
 resource "azurerm_resource_group" "resorce_group" {
-  provider = var.provider != null ? var.provider : azurerm
+  provider = var.provider_dst != null ? var.provider_dst : azurerm
   name     = var.name
   location = var.location
   tags     = var.tags != null ? var.tags : {}
--- modules/azure-resource-groups/variables.tf
+++ modules/azure-resource-groups/variables.tf
@@ -18,9 +18,3 @@ variable "tags" {
   default = {}
   description = "(Optional) A mapping of tags which should be assigned to the Resource Group."
 }
-
-variable "provider" {
-  type    = string
-  default = null
-  description = "(Optional) The provider to use. If not specified, the default provider will be used."
-}
