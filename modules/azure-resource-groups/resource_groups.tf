a0e764d37b421000f36642db2963bd4181a25441
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2023-11-14 11:58:03 +0100

    Test

--- modules/azure-resource-groups/resource_groups.tf
+++ modules/azure-resource-groups/resource_groups.tf
@@ -1,6 +1,8 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
+
+
 resource "azurerm_resource_group" "resorce_group" {
-  provider = var.provider_dst != null ? var.provider_dst : azurerm
+  provider = provider.azurerm
   name     = var.name
   location = var.location
   tags     = var.tags != null ? var.tags : {}
