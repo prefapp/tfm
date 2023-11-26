7d15be7c5a117f973ce626009d1dea42b77b1c25
Antonio Vega <91343444+arvegadev@users.noreply.github.com>
2023-11-26 17:22:24 +0100

    Update locals.tf

--- modules/aws-eks/locals.tf
+++ modules/aws-eks/locals.tf
@@ -1,30 +1,101 @@
-################################################################################
-# Local Variables
-################################################################################
+
+/*
+  ##################################
+  # LOCAL VARIABLES FOR THE ADDONS #
+  ##################################
+
+  > What are local variables for the addons?
+
+    - The local addons variables allows to set the configuration_values
+      for the addons without having to set the whole configuration_values map
+
+  > How does it work?
+
+    - The local variables for the addons are merged with the base_addons variable
+      and then merged with the cluster_addons variable.
+
+  > Can I disable an addon?
+
+    - Yes, you can disable an addon by setting addon_disabled to true
+
+       · Example in your .tfvars:
+
+        cluster_addons = {
+          vpc-cni = {
+            addon_disabled = true
+          }
+        }
+
+  > Can I set the addon version?
+
+    - Yes, you can set the addon version by setting addon_version
+
+       · Example in your .tfvars:
+
+        cluster_addons = {
+          vpc-cni = {
+            addon_version = "v1.7.5-eksbuild.1"
+          }
+        }
+
+  > Can I set the addon configuration_values?
+
+    - Yes, you can set the addon configuration_values, it will overwrite the default values
+
+       · Example in your .tfvars:
+
+        cluster_addons = {
+          vpc-cni = {
+            configuration_values = {
+              env = {
+                ENABLE_PREFIX_DELEGATION = "true"
+                MINIMUM_IP_TARGET        = "20"
+              }
+            }
+          }
+        }
+*/
+
 locals {
 
-  account_id = data.aws_caller_identity.current.account_id
+  base_addons = {
+    vpc-cni    = {},
+    kube-proxy = {},
+    coredns    = {},
+  }
 
-  cluster_addons = merge(
-    {},
+  mixed_addons = merge(
+    local.base_addons,
+    var.cluster_addons
+  )
 
-    var.cluster_addons,
-
+  cluster_addons = { for key, value in merge(
     {
-      for key, value in var.cluster_addons : key => {
+      for key, value in local.mixed_addons : key => {
 
         for k, v in merge(
+          {
+            addon_disabled = false,
 
-          {},
+            configuration_values = (key == "vpc-cni" ? jsonencode({
+
+              env = {
+                ENABLE_PREFIX_DELEGATION = "true"
+                MINIMUM_IP_TARGET        = "8"
+                WARM_IP_TARGET           = "4"
+                WARM_PREFIX_TARGET       = "1"
+              }
+            }) : null)
+
+            resolve_conflicts = "OVERWRITE"
+          },
 
           value,
 
           lookup(value, "configuration_values", null) != null ?
-
           {
             configuration_values = jsonencode(value.configuration_values)
           }
-
           :
 
           {
@@ -33,5 +104,7 @@ locals {
 
       ) : k => v if v != null }
     }
-  )
+
+  ) : key => value if value.addon_disabled != true }
 }
+
