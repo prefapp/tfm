145297152d7d37403b8f972f581cfe6fc8729351
arvegadev <arvegadev@hotmail.com>
2023-11-29 10:43:32 +0100

    fix: example with values

--- modules/aws-eks/_examples/with_yaml_file/main.tf
+++ modules/aws-eks/_examples/with_yaml_file/main.tf
@@ -3,57 +3,57 @@
 
 module "eks_prefapp" {
 
-  source                               = "../.." # Use the local module
+  source = "../.." # Use the local module
 
-  cluster_name                         = local.values.cluster_name
+  cluster_name = local.values.cluster_name
 
-  cluster_version                      = local.values.cluster_version
+  cluster_version = local.values.cluster_version
 
-  node_groups                          = local.values.node_groups
+  node_groups = local.values.node_groups
 
-  region                               = local.values.region
+  region = local.values.region
 
-  alb_ingress_enabled                  = local.values.alb_ingress_enabled
+  create_alb_ingress_iam               = local.values.create_alb_ingress_iam
 
-  cloudwatch_enabled                   = local.values.cloudwatch_enabled
+  create_cloudwatch_iam                = local.values.create_cloudwatch_iam
 
-  efs_driver_enabled                   = local.values.efs_driver_enabled
+  create_efs_driver_iam                = local.values.create_efs_driver_iam
 
-  external_dns_enabled                 = local.values.external_dns_enabled
+  create_external_dns_iam              = local.values.create_external_dns_iam
 
-  parameter_store_enabled              = local.values.parameter_store_enabled
+  create_parameter_store_iam           = local.values.create_parameter_store_iam
 
   node_security_group_additional_rules = local.values.node_security_group_additional_rules
 
-  vpc_id                               = local.values.vpc_id
+  vpc_id = local.values.vpc_id
 
-  subnet_ids                           = local.values.subnet_ids
+  subnet_ids = local.values.subnet_ids
 
-  cluster_tags                         = local.values.cluster_tags
+  tags = local.values.tags
 
-  cluster_addons                       = local.values.cluster_addons
+  cluster_addons = local.values.cluster_addons
 
-  security_groups_ids                  = local.values.security_groups_ids
+  security_groups_ids = local.values.security_groups_ids
 
-  aws_auth_users                       = local.values.aws_auth_users
+  aws_auth_users = local.values.aws_auth_users
 
-  cluster_endpoint_private_access      = local.values.cluster_endpoint_private_access
+  cluster_endpoint_private_access = local.values.cluster_endpoint_private_access
 
-  cluster_endpoint_public_access       = local.values.cluster_endpoint_public_access
+  cluster_endpoint_public_access = local.values.cluster_endpoint_public_access
 
-  cluster_security_group_id            = local.values.cluster_security_group_id
+  cluster_security_group_id = local.values.cluster_security_group_id
 
-  cluster_iam_role_arn                 = local.values.cluster_iam_role_arn
+  cluster_iam_role_arn = local.values.cluster_iam_role_arn
 
-  cluster_encryption_config            = local.values.cluster_encryption_config
+  cluster_encryption_config = local.values.cluster_encryption_config
 
-  create_cluster_iam_role              = local.values.create_cluster_iam_role
+  create_cluster_iam_role = local.values.create_cluster_iam_role
 
-  create_kms_key                       = local.values.create_kms_key
+  create_kms_key = local.values.create_kms_key
 
-  manage_aws_auth_configmap            = local.values.manage_aws_auth_configmap
+  manage_aws_auth_configmap = local.values.manage_aws_auth_configmap
 
-  enable_irsa                          = local.values.enable_irsa
+  enable_irsa = local.values.enable_irsa
 
 }
 
