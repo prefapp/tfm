4eaf974f6fd5fb3b932039102ae3eb0bd1636e5e
Javier Gómez Rodríguez <javier.gomez@prefapp.es>
2024-01-25 18:46:27 +0100

    fix: if inputs are provided do not use data

--- modules/azure-aks/data.tf
+++ modules/azure-aks/data.tf
@@ -1,5 +1,6 @@
 #https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/subnet
 data "azurerm_subnet" "aks_subnet" {
+  count                = var.subnet_id ? 0 : 1
   name                 = var.aks_subnet_name
   virtual_network_name = var.aks_vnet_name
   resource_group_name  = var.aks_vnet_name_resource_group
