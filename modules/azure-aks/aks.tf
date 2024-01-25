cf2f9a2b31f9dc7506e1e72b6d0b51106cf6ff35
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2024-01-25 15:00:43 +0100

    Foo

--- modules/azure-aks/aks.tf
+++ modules/azure-aks/aks.tf
@@ -39,7 +39,7 @@ resource "azurerm_kubernetes_cluster" "kubernetes" {
 lifecycle {
   ignore_changes = [
     "default_node_pool[0].node_count",
-    var.ignored_tags,
+    var.ignored_tags
   ]
 }
 }
