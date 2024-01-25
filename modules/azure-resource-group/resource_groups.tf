b08c1f8f90afa6cdbad04b5b240674fd91dfe4ed
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2024-01-25 17:12:01 +0100

    Add lifecycle to ignore certain key tags into azure-resource-group module

--- modules/azure-resource-group/resource_groups.tf
+++ modules/azure-resource-group/resource_groups.tf
@@ -3,4 +3,11 @@ resource "azurerm_resource_group" "resorce_group" {
   name     = var.name
   location = var.location
   tags     = var.tags != null ? var.tags : {}
+  lifecycle {
+    ignore_changes = [
+      tags["cliente"],
+      tags["producto"],
+      tags["env"]
+    ]
+  }
 }
