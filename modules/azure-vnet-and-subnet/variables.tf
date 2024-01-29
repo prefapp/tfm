d5cbfb422f54103e7effbbbdb4a8729d7b7e76e2
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2024-01-29 23:17:04 +0100

    Test NSGs

--- modules/azure-vnet-and-subnet/variables.tf
+++ modules/azure-vnet-and-subnet/variables.tf
@@ -40,6 +40,20 @@ variable "subnets" {
         actions = list(string)
       })
     })))
+    network_security_group = optional(object({
+      name = string
+      security_rules = list(object({
+        name                       = string
+        priority                   = number
+        direction                  = string
+        access                     = string
+        protocol                   = string
+        source_port_range          = string
+        destination_port_range     = string
+        source_address_prefix      = string
+        destination_address_prefix = string
+      }))
+    }))
   }))
   default = {}
 }
