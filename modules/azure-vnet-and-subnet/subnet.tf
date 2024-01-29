63d5340b12f32cb5a973bd6ecbe235ce236a93ec
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2024-01-29 23:24:35 +0100

    Test NSGs

--- modules/azure-vnet-and-subnet/subnet.tf
+++ modules/azure-vnet-and-subnet/subnet.tf
@@ -51,3 +51,12 @@ resource "azurerm_network_security_group" "nsg" {
     }
   }
 }
+
+# Resource block for creating Azure network security group associations
+resource "azurerm_subnet_network_security_group_association" "association" {
+  for_each = { for subnet_name, subnet in var.subnets : subnet_name => subnet if subnet.network_security_group != null }
+
+  subnet_id                 = azurerm_subnet.subnet[each.key].id
+  network_security_group_id = azurerm_network_security_group.nsg[each.key].id
+}
+
