3430049544ea3aef301f7a27fb2795a5ee105fb4
Toni R. Vega <91343444+tonirvega@users.noreply.github.com>
2024-06-24 18:53:34 +0200

    Update directory_roles.tf

--- modules/azuread-group/directory_roles.tf
+++ modules/azuread-group/directory_roles.tf
@@ -3,7 +3,7 @@ data "azuread_directory_roles" "current" {}
 
 # https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/directory_role_assignment
 resource "azuread_directory_role_assignment" "this" {
-# { for role in local.flattened_role_assingments : role.name => role }
+
   for_each = {for directory_role in var.directory_roles : directory_role.role_name => directory_role }
 
   role_id             = [for role in data.azuread_directory_roles.current.roles : role if role.display_name == each.value.role_name][0].template_id
