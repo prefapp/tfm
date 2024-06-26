11eb446db1c3e05b76ebf581fa0d41618bd5b62a
tonirvega <antonio.rodriguez@prefapp.es>
2024-06-26 17:04:25 +0200

    fix

--- modules/azuread-group/pim_eligible.tf
+++ modules/azuread-group/pim_eligible.tf
@@ -34,7 +34,7 @@ resource "azuread_privileged_access_group_eligibility_schedule" "owners" {
 
     assignment_type = "owner"
 
-    #duration        = "PT${each.value.pim_expiration_hours != null ? each.value.pim_expiration_hours : var.default_pim_duration}H"
+    duration        = each.value.permanent_assignment != null ? null : ("PT${each.value.pim_expiration_hours != null  ? each.value.pim_expiration_hours : var.default_pim_duration}H")
 
     permanent_assignment = each.value.permanent_assignment != null ? each.value.permanent_assignment : false
 
