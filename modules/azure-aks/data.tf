d4e1c878a7691b266170397cc8f5384af28985c4
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2024-07-30 15:16:46 +0200

    Change aks module to use official aks module

--- modules/azure-aks/data.tf
+++ modules/azure-aks/data.tf
@@ -1,6 +1,11 @@
-#https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/subnet
+# Data section
 data "azurerm_subnet" "aks_subnet" {
-  name                 = var.aks_subnet_name
-  virtual_network_name = var.aks_vnet_name
-  resource_group_name  = var.aks_vnet_name_resource_group
+  name                 = var.subnet_name
+  virtual_network_name = var.vnet_name
+  resource_group_name  = var.vnet_resource_group_name
+}
+
+data "azurerm_public_ip" "aks_public_ip" {
+  name                = var.public_ip_name
+  resource_group_name = var.resource_group_name
 }
--- modules/azure-aks/main.tf
+++ modules/azure-aks/main.tf
@@ -1,8 +1,12 @@
 terraform {
+  required_version = ">= 1.7.1"
+
   required_providers {
+    azapi = {
+      source  = "Azure/azapi"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.79.0"
     }
   }
 }
