fd11451404df588d6d94708d2bd4e9207946515f
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2024-08-22 16:30:48 +0200

    Remove yaml references

--- modules/azure-mi/main.tf
+++ modules/azure-mi/main.tf
@@ -1,14 +1,13 @@
 # RESOURCES SECTION
 ## https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/resource_group.html
 data "azurerm_resource_group" "resource_group" {
-  name = local.vars_yaml.resource_group_name
+  name = var.resource_group_name
 }
 
 ## LOCALS SECTION
 locals {
-  vars_yaml = yamldecode(file("vars.yaml"))
   rbac = [
-    for r in local.vars_yaml.rbac : [
+    for r in var.rbac : [
       for role in r.roles : {
         name  = r.name
         scope = r.scope
@@ -17,15 +16,15 @@ locals {
     ]
   ]
   flattened_rbac = flatten(local.rbac)
-  tags = contains(keys(local.vars_yaml), "tags_from_rg") ? data.azurerm_resource_group.resource_group.tags : local.vars_yaml.tags
+  tags = var.tags_from_rg ? data.azurerm_resource_group.resource_group.tags : var.tags
 }
 
 ## RESOURCES SECTION
 ## https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/user_assigned_identity
 resource "azurerm_user_assigned_identity" "user_assigned_identity" {
-  name                = local.vars_yaml.name
-  location            = local.vars_yaml.location
-  resource_group_name = local.vars_yaml.resource_group_name
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   tags                = local.tags
 }
 
@@ -39,13 +38,11 @@ resource "azurerm_role_assignment" "role_assignment" {
 
 ## https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/federated_identity_credential
 resource "azurerm_federated_identity_credential" "federated_identity_credential" {
-  for_each            = { for federated_credential in local.vars_yaml.federated_credentials : federated_credential.name => federated_credential }
+  for_each            = { for federated_credential in var.federated_credentials : federated_credential.name => federated_credential }
   name                = each.key
-  resource_group_name = local.vars_yaml.resource_group_name
+  resource_group_name = var.resource_group_name
   audience            = ["api://AzureADTokenExchange"]
   issuer              = each.value.issuer
   parent_id           = azurerm_user_assigned_identity.user_assigned_identity.id
-  # subject github example: repo:{Organization}/{Repository}:{Entity}
-  # subject k8s example: system:serviceaccount:<namespace>:<serviceaccount>
   subject = each.value.type == "github" ? "repo:${each.value.organization}/${each.value.repository}:${each.value.entity}" : "system:serviceaccount:${each.value.namespace}:${each.value.service_account_name}"
 }
