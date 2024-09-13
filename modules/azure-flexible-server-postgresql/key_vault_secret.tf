62080fd0e7b42a5490da5e141da85e7906d61e5f
Juan CFF <97174951+jcframil@users.noreply.github.com>
2024-09-13 16:41:37 +0200

    fix: avoid validations (#158)
    * avoid validations
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * update documentation
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    ---------
    
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-flexible-server-postgresql/key_vault_secret.tf
+++ modules/azure-flexible-server-postgresql/key_vault_secret.tf
@@ -1,32 +1,23 @@
-# We only generate the password if necessary
-resource "random_password" "password" {
-  count   = local.data.password.create ? 1 : 0
-  length  = 20
-  special = true
-}
-
-# We only upload the password if necessary
-# Once created we ignore it
-resource "azurerm_key_vault_secret" "password_create" {
-  count        = local.data.password.create ? 1 : 0
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-  name         = local.data.password.key_vault_secret_name
-  value        = random_password.password[0].result ? random_password.password[0].result : null
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-# Checkers and data access
+# Data source to get the Key Vault details
 data "azurerm_key_vault" "key_vault" {
   name                = local.data.password.key_vault_name
   resource_group_name = local.data.password.key_vault_resource_group
 }
 
-data "azurerm_key_vault_secret" "password" {
-  name         = local.data.password.key_vault_secret_name
+# Always create the random password (even if it may not be used)
+resource "random_password" "password" {
+  length  = 20
+  special = true
+}
+
+# Create the Key Vault secret
+resource "azurerm_key_vault_secret" "password_create" {
   key_vault_id = data.azurerm_key_vault.key_vault.id
-  depends_on = [
-    azurerm_key_vault_secret.password_create
-  ]
+  name         = local.data.password.key_vault_secret_name
+  value        = random_password.password.result
+  depends_on = [ random_password.password ]
+  lifecycle {
+    # Ignore changes to the secret's value to prevent overwriting it after the initial creation
+    ignore_changes = [value]
+  }
 }
