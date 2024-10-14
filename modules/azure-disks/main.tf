00b91c6f2d119b94f8437c0bdf5f8ba1e85b2ab6
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2024-10-14 17:57:59 +0200

    Add azure-disks-module

--- modules/azure-disks/main.tf
+++ modules/azure-disks/main.tf
@@ -1,12 +1,12 @@
-# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/managed_disk
 resource "azurerm_managed_disk" "disks" {
-  for_each             = var.disks
+  for_each             = { for disk in var.disks : disk.name => disk }
   name                 = each.key
   location             = var.location
   resource_group_name  = var.resource_group_name
-  storage_account_type = each.value.storage_account_type
+  storage_account_type = lookup(each.value, "storage_account_type", "Standard_LRS")
   create_option        = lookup(each.value, "create_option", "Empty")
-  disk_size_gb         = 4
+  disk_size_gb         = lookup(each.value, "disk_size_gb", 4)
+
   lifecycle {
     ignore_changes = [
       tags,
--- modules/azure-disks/variables.tf
+++ modules/azure-disks/variables.tf
@@ -1,19 +1,14 @@
 # Variable section
-variable "environment" {
-  type = string
-}
-
 variable "location" {
+  description = "The Azure region where the managed disk should be created."
   type = string
 }
 
 variable "resource_group_name" {
+  description = "The name of the resource group in which the managed disk should be created."
   type = string
 }
 
 variable "disks" {
   description = "A map of managed disk configurations."
-  type = map(object({
-    storage_account_type = string
-  }))
 }
