029131d15e38d1ca5570a94941ac5418394515d3
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2024-10-14 17:41:20 +0200

    Add azure-disks-module

--- modules/azure-disks/outputs.tf
+++ modules/azure-disks/outputs.tf
@@ -1,8 +1,8 @@
 # Output section
 output "disk_names" {
-  value = [for disk in azurerm_managed_disk.example : disk.name]
+  value = [for disk in azurerm_managed_disk.disks : disk.name]
 }
 
 output "disk_ids" {
-  value = [for disk in azurerm_managed_disk.example : disk.id]
+  value = [for disk in azurerm_managed_disk.disks : disk.id]
 }
