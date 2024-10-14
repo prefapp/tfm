3fbd9b7d51c65a3dbeb7d6dd03dfcc64d3f4ea61
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2024-10-14 18:46:12 +0200

    Add azure-disks-module

--- modules/azure-disks/main.tf
+++ modules/azure-disks/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_managed_disk" "disks" {
   name                 = each.key
   location             = var.location
   resource_group_name  = var.resource_group_name
-  storage_account_type = lookup(each.value, "storage_account_type", "Standard_LRS")
+  storage_account_type = lookup(each.value, "storage_account_type", "StandardSSD_LRS")
   create_option        = lookup(each.value, "create_option", "Empty")
   disk_size_gb         = lookup(each.value, "disk_size_gb", 4)
 
