52f79ebd2b2bed1121c316b28389e33adc564bdb
Gustavo Esteban Borragán <68920322+gustavoborragan@users.noreply.github.com>
2024-10-17 12:04:05 +0200

    Update role_assignment.tf

--- modules/azure-aks/role_assignment.tf
+++ modules/azure-aks/role_assignment.tf
@@ -1,5 +1,5 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/role_assignment
-resource "azurerm_role_assignment" "role_assignment_network_contributor_over_public_ip_aks_predev" {
+resource "azurerm_role_assignment" "role_assignment_network_contributor_over_public_ip_aks" {
     count               = var.create_role_assignment_public_ip ? 1 : 0
     scope               = data.azurerm_public_ip.aks_public_ip.id
     role_definition_name = "Network Contributor"
