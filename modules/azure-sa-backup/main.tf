aa06e5ef63e0c37eb3a0fb47a7fca4952f26d5e2
jcframil <framiljuan@gmail.com>
2024-10-18 16:35:57 +0200

    fix backup_shares and backup_blobs optional vars
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-sa-backup/main.tf
+++ modules/azure-sa-backup/main.tf
@@ -125,7 +125,7 @@ resource "azurerm_data_protection_backup_vault" "this" {
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/role_assignment
 resource "azurerm_role_assignment" "this" {
-  count               = var.backup_blob != null && var.backup_blob.identity_type != null ? 1 : 0
+  count               = var.backup_blob.identity_type != null ? 1 : 0
   scope               = var.storage_account_id
   role_definition_name = var.backup_blob.role_assignment
   principal_id        = azurerm_data_protection_backup_vault.this[0].identity[0].principal_id
