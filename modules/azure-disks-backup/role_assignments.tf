47109a7805608206c04e48573f7b62c15689e33b
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2024-10-18 10:38:59 +0200

    Module disks backups

--- modules/azure-disks-backup/role_assignments.tf
+++ modules/azure-disks-backup/role_assignments.tf
@@ -1,7 +1,7 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/role_assignment
 resource "azurerm_role_assignment" "this_rg" {
     scope                = data.azurerm_resource_group.this.id
-    role_definition_name = "Disk Backup Reader"
+    role_definition_name = "Disk Snapshot Contributor"
     principal_id         = azurerm_data_protection_backup_vault.this.identity[0].principal_id
     depends_on = [
         azurerm_data_protection_backup_vault.this
