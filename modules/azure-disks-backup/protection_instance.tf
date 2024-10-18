aea7028bea3fbcef2d92ee63f9e369699309076c
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2024-10-18 19:29:17 +0200

    Fix some variables

--- modules/azure-disks-backup/protection_instance.tf
+++ modules/azure-disks-backup/protection_instance.tf
@@ -2,7 +2,7 @@
 resource "azurerm_data_protection_backup_instance_disk" "this" {
     for_each                     = { for instance in var.backup_instances : instance.disk_name => instance }
     name                         = each.key
-    location                     = data.azurerm_managed_disk.this[each.key].location
+    location                     = data.azurerm_managed_disk.this[each.key].region
     vault_id                     = azurerm_data_protection_backup_vault.this.id
     disk_id                      = data.azurerm_managed_disk.this[each.key].id
     snapshot_resource_group_name = each.value.snapshot_resource_group_name
