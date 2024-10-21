20d2499137da5e9e76458a049372a935554dabf6
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2024-10-21 10:45:39 +0200

    Fix some variables

--- modules/azure-disks-backup/data.tf
+++ modules/azure-disks-backup/data.tf
@@ -10,16 +10,16 @@ data "azurerm_managed_disk" "this" {
   resource_group_name = each.value.disk_resource_group
 }
 
-# Ensure none of the disks have the same resource group as the value of var.resource_group_name
-resource "null_resource" "validate_disks" {
-  count = length([for instance in var.backup_instances : instance if instance.disk_resource_group == var.resource_group_name])
+# # Ensure none of the disks have the same resource group as the value of var.resource_group_name
+# resource "null_resource" "validate_disks" {
+#   count = length([for instance in var.backup_instances : instance if instance.disk_resource_group == var.resource_group_name])
 
-  provisioner "local-exec" {
-    command = "echo 'Validation failed: One or more disks have the same resource group as resource group vault'"
-    when    = "create"
-  }
+#   provisioner "local-exec" {
+#     command = "echo 'Validation failed: One or more disks have the same resource group as resource group vault'"
+#     when    = "create"
+#   }
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
