2fd3e1ebe01478787ca373aff57ba75cb75ceb00
Juan CFF <97174951+jcframil@users.noreply.github.com>
2024-10-24 10:55:39 +0200

    fix: add backups separate rg control (#361)
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-sa-backup/README.md
+++ modules/azure-sa-backup/README.md
@@ -29,7 +29,8 @@
 
 | Name | Description | Type | Default | Required |
 |------|-------------|------|---------|:--------:|
-| <a name="input_resource_group_name"></a> [resource\_group\_name](#input\_resource\_group\_name) | The name for the resource group | `string` | n/a | yes |
+| <a name="input_storage_account_resource_group_name"></a> [storage\_account\_resource\_group\_name](#input\_storage\_account\_resource\_group\_name) | The name for the resource group for the storage account | `string` | n/a | yes |
+| <a name="input_backup_resource_group_name"></a> [backup\_resource\_group\_name](#input\_backup\_resource\_group\_name) | The name for the resource group for the backups | `string` | n/a | yes |
 | <a name="input_storage_account_id"></a> [storage\_account\_id](#input\_storage\_account\_id) | The ID of the storage account | `string` | n/a | yes |
 | <a name="input_backup_share"></a> [backup\_share](#input\_backup\_share) | Specifies the backup configuration for the storage share | `object({ policy_name = string, recovery_services_vault_name = string, sku = string, soft_delete_enabled = optional(bool), storage_mode_type = optional(string, "GeoRedundant"), cross_region_restore_enabled = optional(bool), source_file_share_name = list(string), identity = optional(object({ type = optional(string, "SystemAssigned"), identity_ids = optional(list(string), []) })), encryption = optional(object({ key_id = optional(string, null), infrastructure_encryption_enabled = optional(bool, false), user_assigned_identity_id = optional(string, null), use_system_assigned_identity = optional(bool, false) })), timezone = optional(string), backup = object({ frequency = string, time = string }), retention_daily = object({ count = number }), retention_weekly = optional(object({ count = number, weekdays = optional(list(string), ["Sunday"]) })), retention_monthly = optional(object({ count = number, weekdays = optional(list(string), ["Sunday"]), weeks = optional(list(string), ["First"]), days = optional(list(number)) })), retention_yearly = optional(object({ count = number, months = optional(list(string), ["January"]), weekdays = optional(list(string), ["Sunday"]), weeks = optional(list(string), ["First"]), days = optional(list(number)) })) })` | null | no |
 | <a name="input_backup_blob"></a> [backup\_blob](#input\_backup\_blob) | Specifies the backup configuration for the storage blob | `object({ vault_name = string, datastore_type = string, redundancy = string, identity_type = optional(string), role_assignment = string, instance_blob_name = string, storage_account_container_names = optional(list(string)), policy = object({ name = string, backup_repeating_time_intervals = optional(list(string)), operational_default_retention_duration = optional(string), retention_rule = optional(list(object({ name = string, duration = string, criteria = object({ absolute_criteria = optional(string), days_of_month = optional(list(number)), days_of_week = optional(list(string)), months_of_year = optional(list(string)), scheduled_backup_times = optional(list(string)), weeks_of_month = optional(list(string)) }), life_cycle = object({ data_store_type = string, duration = string }), priority = number }))), time_zone = optional(string), vault_default_retention_duration = optional(string), retention_duration = optional(string) }) })` | null | no |
@@ -47,9 +48,10 @@
 ```yaml
     values:
       # General values
-      resource_group_name: "test-rg"
-      storage_account_id: "/subscriptions/xxx/resourceGroups/xxx/providers/Microsoft.Storage/storageAccounts/xxx"
-
+      storage_account_resource_group_name: "test-rg"
+      backup_resource_group_name: "backup-test-rg"
+      storage_account_id: "/subscriptions/xxx/resourceGroups/xxx/providers/Microsoft.Storage/storageAccounts/xxx" # can use refs ${{tfworkspace:claim-name:outputs.id}}
+      
       # Backup share values
       backup_share:
         policy_name: "daily-backup-policy"
---
+++ modules/azure-sa-backup/shares.tf
@@ -0,0 +1,96 @@
+## BACKUPS FILE SHARES
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/recovery_services_vault
+resource "azurerm_recovery_services_vault" "this" {
+  count                        = var.backup_share != null ? 1 : 0
+  name                         = var.backup_share.recovery_services_vault_name
+  resource_group_name          = var.backup_resource_group_name
+  location                     = azurerm_resource_group.this.location
+  sku                          = var.backup_share.sku
+  soft_delete_enabled          = var.backup_share.soft_delete_enabled
+  storage_mode_type            = var.backup_share.storage_mode_type
+  cross_region_restore_enabled = var.backup_share.cross_region_restore_enabled
+  tags                         = azurerm_resource_group.this.tags
+  dynamic "identity" {
+    for_each = var.backup_share.identity != null ? [1] : []
+    content {
+      type         = var.backup_share.identity.type
+      identity_ids = var.backup_share.identity.identity_ids
+    }
+  }
+  dynamic "encryption" {
+    for_each = var.backup_share.encryption != null ? [1] : []
+    content {
+      key_id                            = var.backup_share.encryption.key_id
+      infrastructure_encryption_enabled = var.backup_share.encryption.infrastructure_encryption_enabled
+      user_assigned_identity_id         = var.backup_share.encryption.user_assigned_identity_id
+      use_system_assigned_identity      = var.backup_share.encryption.use_system_assigned_identity
+    }
+  }
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/backup_container_storage_account
+resource "azurerm_backup_container_storage_account" "this" {
+  count               = var.backup_share != null ? 1 : 0
+  resource_group_name = var.backup_resource_group_name
+  recovery_vault_name = var.backup_share.recovery_services_vault_name
+  storage_account_id  = var.storage_account_id
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/backup_policy_file_share
+resource "azurerm_backup_policy_file_share" "this" {
+  count               = var.backup_share != null ? 1 : 0
+  name                = var.backup_share.policy_name
+  resource_group_name = var.backup_resource_group_name
+  recovery_vault_name = var.backup_share.recovery_services_vault_name
+  timezone            = var.backup_share.timezone
+  backup {
+    frequency = var.backup_share.backup.frequency
+    time      = var.backup_share.backup.time
+  }
+
+  dynamic "retention_daily" {
+    for_each = var.backup_share.retention_daily != null ? [1] : []
+    content {
+      count = var.backup_share.retention_daily.count
+    }
+  }
+  dynamic "retention_weekly" {
+    for_each = var.backup_share.retention_weekly != null ? [1] : []
+    content {
+      count    = var.backup_share.retention_weekly.count
+      weekdays = var.backup_share.retention_weekly.weekdays
+    }
+  }
+  dynamic "retention_monthly" {
+    for_each = var.backup_share.retention_monthly != null ? [1] : []
+    content {
+      count    = var.backup_share.retention_monthly.count
+      weekdays = var.backup_share.retention_monthly.weekdays
+      weeks    = var.backup_share.retention_monthly.weeks
+      days     = var.backup_share.retention_monthly.days
+    }
+  }
+  dynamic "retention_yearly" {
+    for_each = var.backup_share.retention_yearly != null ? [1] : []
+    content {
+      count    = var.backup_share.retention_yearly.count
+      weekdays = var.backup_share.retention_yearly.weekdays
+      weeks    = var.backup_share.retention_yearly.weeks
+      months   = var.backup_share.retention_yearly.months
+
+    }
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/backup_protected_file_share
+resource "azurerm_backup_protected_file_share" "this" {
+  count                     = var.backup_share != null ? length(var.backup_share.source_file_share_name) : 0
+  resource_group_name       = var.backup_resource_group_name
+  recovery_vault_name       = var.backup_share.recovery_services_vault_name
+  source_storage_account_id = var.storage_account_id
+  source_file_share_name    = element(var.backup_share.source_file_share_name, count.index)
+  backup_policy_id          = azurerm_backup_policy_file_share.this[count.index].id
+}
