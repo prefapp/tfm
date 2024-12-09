b6a5845986677f3371a5d92e1da0d75cd9a24c77
JulianGV21 <llulian.gomez@gmail.com>
2024-12-09 18:29:47 +0100

    Adding cloudamqp-cluster module

---
+++ modules/cloudamqp-cluster/outputs.tf
@@ -0,0 +1,15 @@
+output name {
+  value = cloudamqp_instance.instance.id
+}
+
+output "cloudamqp_instance_rqm_version" {
+    value = cloudamqp_instance.instance.rqm_version
+}
+
+output "instance_host" {
+    value = cloudamqp_instance.instance.host
+}
+
+output "instance_vhost" {
+    value = cloudamqp_instance.instance.vhost
+}
