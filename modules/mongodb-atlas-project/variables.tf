d7a2dddaca81259e102379b7312ebdac530999b1
Gustavo Esteban Borragán <68920322+gustavoborragan@users.noreply.github.com>
2024-12-18 10:04:19 +0100

    Update variables.tf

--- modules/mongodb-atlas-project/variables.tf
+++ modules/mongodb-atlas-project/variables.tf
@@ -21,28 +21,6 @@ variable "project_name" {
   type        = string
 }
 
-# Cluster seccion variables
-variable "clusters" {
-  description = "A map of clusters to create"
-  type = map(object({
-    name                         = string
-    cluster_type                 = string
-    num_shards                   = number
-    zone_name                    = string
-    region_name                  = string
-    analytics_nodes              = number
-    electable_nodes              = number
-    priority                     = number
-    read_only_nodes              = number
-    cloud_backup                 = bool
-    auto_scaling_disk_gb_enabled = bool
-    mongo_db_major_version       = string
-    provider_name                = string
-    provider_disk_type_name      = string
-    provider_instance_size_name  = string
-  }))
-}
-
 # Users seccion variables
 variable "database_users" {
   description = "A map of database users to create"
