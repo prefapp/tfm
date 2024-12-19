25a90aafcd3b6a4316a65bc7defc2ce52ad4a2b6
Gustavo Esteban Borragán <68920322+gustavoborragan@users.noreply.github.com>
2024-12-19 09:13:18 +0100

    feat: mongodb-atlas-cluster module (#386)
    * mongodb-atlas-cluster module
    
    * Update versions.tf
    
    * Add feature create from from backup
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Migrate to mongo modules on corpme claims

---
+++ modules/mongodb-atlas-cluster/variables.tf
@@ -0,0 +1,113 @@
+# Global variables
+variable "mongo_region" {
+  description = "The mongo region"
+  type        = string
+}
+
+variable "provider_name" {
+  description = "The provider name"
+  type        = string
+}
+
+variable "project_id" {
+  description = "The project ID"
+  type        = string
+}
+
+# Cluster seccion variables
+variable "cluster_name" {
+  description = "The name of the cluster"
+  type        = string
+}
+
+variable "cluster_type" {
+  description = "The type of the cluster"
+  type        = string
+}
+
+variable "cluster_replication_specs_config_analytics_nodes" {
+  description = "The number of analytics nodes"
+  type        = number
+}
+
+variable "cluster_replication_specs_config_electable_nodes" {
+  description = "The number of electable nodes"
+  type        = number
+}
+
+variable "cluster_replication_specs_config_priority" {
+  description = "Priority value"
+  type        = number
+}
+
+variable "cluster_replication_specs_config_read_only_nodes" {
+  description = "The number of read only nodes"
+  type        = number
+}
+
+variable "cluster_cloud_backup" {
+  description = "Whether or not cloud backup is enabled"
+  type        = bool
+}
+
+variable "cluster_auto_scaling_disk_gb_enabled" {
+  description = "Whether or not disk autoscaling is enabled"
+  type        = bool
+}
+
+variable "cluster_mongo_db_major_version" {
+  description = "MongoDB major version"
+  type        = string
+}
+
+variable "cluster_provider_disk_type_name" {
+  description = "Provider disk type name"
+  type        = string
+}
+
+variable "cluster_provider_instance_size_name" {
+  description = "Provider instance size name"
+  type        = string
+}
+
+variable "cluster_num_shards" {
+  description = "The number of shards"
+  type        = number
+}
+
+variable "cluster_zone" {
+  description = "The zones of the cluster"
+  type        = string
+}
+
+# Restore from snapshot variables
+variable "create_cluster_from_snapshot" {
+  description = "Whether or not to create a cluster from a snapshot"
+  type        = bool
+  default     = false
+}
+
+variable "origin_project_id" {
+  description = "The origin project ID"
+  type        = string
+  default     = null
+}
+
+variable "origin_cluster_name" {
+  description = "The origin cluster name"
+  type        = string
+  default     = null
+}
+
+# Restore from pitr variables
+variable "create_cluster_from_pitr" {
+  description = "Whether or not to create a cluster from a point in time restore"
+  type        = bool
+  default     = false
+}
+
+variable "point_in_time_utc_seconds" {
+  description = "The point in time to restore from"
+  type        = number
+  default     = null
+}
