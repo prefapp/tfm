dc9c8947add86d1ec218307a2084c717900a4c35
JulianGV21 <llulian.gomez@gmail.com>
2024-12-20 14:37:35 +0100

    Split main to instance.tf

---
+++ modules/cloudamqp-cluster/instance.tf
@@ -0,0 +1,10 @@
+# https://registry.terraform.io/providers/cloudamqp/cloudamqp/1.32.2/docs/resources/instance
+resource "cloudamqp_instance" "this" {
+  name              = var.cloudamqp_instance.name
+  plan              = var.cloudamqp_instance.plan
+  region            = var.cloudamqp_instance.region
+  tags              = var.cloudamqp_instance.tags
+  nodes             = var.cloudamqp_instance.nodes
+  rmq_version       = var.cloudamqp_instance.rmq_version
+  no_default_alarms = true
+}
