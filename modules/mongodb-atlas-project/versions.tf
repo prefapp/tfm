6b85e085b9dfcc1f75fb47b430203abbeba3b4e5
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2025-01-13 21:49:00 +0100

    Add random user password mongodb atlas projet module

--- modules/mongodb-atlas-project/versions.tf
+++ modules/mongodb-atlas-project/versions.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = ">= 1.7.0"
   required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0.0"
+    }
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
       version = ">= 1.23.0"
