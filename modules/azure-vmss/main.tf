5a98daf599da166a74e4aa8cf98a353bbb1268f2
Juan CFF <97174951+jcframil@users.noreply.github.com>
2025-01-16 18:13:01 +0100

    Update main.tf

--- modules/azure-vmss/main.tf
+++ modules/azure-vmss/main.tf
@@ -116,7 +116,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "this" {
   depends_on = [
     data.azurerm_network_security_group.this,
     data.azurerm_user_assigned_identity.this,
-    azurerm_public_ip_prefix.this
+    azurerm_public_ip_prefix[0].this
   ]
 }
 
