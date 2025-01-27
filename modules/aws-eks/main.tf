1c0abc014a3e89bf3de3ec780a7871d6c4b7ddab
Toni R. Vega <91343444+tonirvega@users.noreply.github.com>
2025-01-27 17:54:29 +0100

    feat: update eks module to v20.33.1
    * feat: update eks module
    
    * Update main.tf

--- modules/aws-eks/main.tf
+++ modules/aws-eks/main.tf
@@ -15,55 +15,35 @@ locals {
 
 # EKS Cluster Configuration
 module "eks" {
-
-  version = "19.20.0"
-
+  version = "20.33.1"
   source = "terraform-aws-modules/eks/aws"
-
   cluster_name = var.cluster_name
-
   cluster_version = var.cluster_version
-
   cluster_endpoint_private_access = var.cluster_endpoint_private_access
-
   cluster_endpoint_public_access = var.cluster_endpoint_public_access
-
   cloudwatch_log_group_retention_in_days = var.cloudwatch_log_group_retention_in_days
-
   vpc_id = var.vpc_id
-
   subnet_ids = var.subnet_ids
-
   create_cluster_security_group = var.create_cluster_security_group
-
   cluster_security_group_id = var.cluster_security_group_id
-
   iam_role_arn = var.cluster_iam_role_arn
-
   create_iam_role = var.create_cluster_iam_role
-
   enable_irsa = var.enable_irsa
-
   eks_managed_node_groups = var.node_groups
-
   node_security_group_additional_rules = var.node_security_group_additional_rules
-
   tags = var.tags
-
   cluster_tags = merge(var.cluster_tags, var.tags)
-
   cluster_addons = local.cluster_addons
-
   create_kms_key = var.create_kms_key
-
   cluster_encryption_config = var.cluster_encryption_config
-
-  aws_auth_users = var.aws_auth_users
-
-  aws_auth_roles = var.aws_auth_roles
-
-  manage_aws_auth_configmap = var.manage_aws_auth_configmap
-
+  access_entries = {}
   fargate_profiles = var.fargate_profiles
+}
 
+module "eks_auth" {
+  source  = "terraform-aws-modules/eks/aws//modules/aws-auth"
+  version = "20.33.1"
+  manage_aws_auth_configmap = true
+  aws_auth_roles = var.aws_auth_roles
+  aws_auth_users    = var.aws_auth_users
 }
