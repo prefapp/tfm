5b0ae3414c3fc4dfa34044f53d55e5c07aa5d4b9
JulianGV21 <llulian.gomez@gmail.com>
2025-01-28 14:49:29 +0100

    Added new module for nsg and nsr

--- modules/azure-nsg-nsr/variables.tf
+++ modules/azure-nsg-nsr/variables.tf
@@ -11,22 +11,6 @@ variable "nsg" {
 
 variable "rules" {
   description = "Network Security Rule configuration"
-  type = map(object({
-    name                         = string
-    priority                     = number
-    direction                    = string
-    access                       = string
-    protocol                     = string
-    source_port_range            = optional(string)
-    source_port_ranges           = optional(list(string))
-    destination_port_range       = optional(string)
-    destination_port_ranges      = optional(list(string))
-    source_address_prefix        = optional(string)
-    source_address_prefixes      = optional(list(string))
-    destination_address_prefix   = optional(string)
-    destination_address_prefixes = optional(list(string))
-    network_security_group_name  = string
-    resource_group_name          = string
-  }))
+  type = map(any)
 }
 
