43dfa7a410867e73a72d4e29a346c5785f696319
JulianGV21 <llulian.gomez@gmail.com>
2025-01-28 14:54:49 +0100

    Added new module for nsg and nsr

--- modules/azure-nsg-nsr/main.tf
+++ modules/azure-nsg-nsr/main.tf
@@ -15,14 +15,6 @@ resource "azurerm_network_security_rule" "this" {
   direction                    = each.value.direction
   access                       = each.value.access
   protocol                     = each.value.protocol
-  source_port_range            = each.value.source_port_range
-  source_port_ranges           = each.value.source_port_ranges
-  destination_port_range       = each.value.destination_port_range
-  destination_port_ranges      = each.value.destination_port_ranges
-  source_address_prefix        = each.value.source_address_prefix
-  source_address_prefixes      = each.value.source_address_prefixes
-  destination_address_prefix   = each.value.destination_address_prefix
-  destination_address_prefixes = each.value.destination_address_prefixes
   resource_group_name          = var.nsg.resource_group_name
   network_security_group_name  = var.nsg.name
 }
