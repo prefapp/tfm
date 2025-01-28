e2a460378e86cccd10ea2663c0e15b9e4dd35f64
JulianGV21 <llulian.gomez@gmail.com>
2025-01-28 17:45:09 +0100

    Updated readme.me and added validations

--- modules/azure-nsg-nsr/main.tf
+++ modules/azure-nsg-nsr/main.tf
@@ -23,6 +23,6 @@ resource "azurerm_network_security_rule" "this" {
   source_address_prefixes      = each.value.source_address_prefixes
   destination_address_prefix   = each.value.destination_address_prefix
   destination_address_prefixes = each.value.destination_address_prefixes
-  resource_group_name          = var.nsg.resource_group_name
-  network_security_group_name  = var.nsg.name
+  resource_group_name          = var.rule.resource_group_name
+  network_security_group_name  = var.rule.network_security_group_name
 }
