94631752a8baa2f3036c27f3c403613dc9196f8c
Julián <167460039+JulianGV21@users.noreply.github.com>
2025-01-29 11:34:14 +0100

    Added new module for nsg and nsr (#423)
    * Added new module for nsg and nsr
    
    * Added new module for nsg and nsr
    
    * Added new module for nsg and nsr
    
    * Added new module for nsg and nsr
    
    * Added new module for nsg and nsr
    
    * Added new module for nsg and nsr
    
    * Added new module for nsg and nsr
    
    * Added new module for nsg and nsr
    
    * Added new module for nsg and nsr
    
    * Added new module for nsg and nsr
    
    * Added new module for nsg and nsr
    
    * Added new module for nsg and nsr
    
    * Updated readme.me and added validations
    
    * Updated readme.me and added validations
    
    * Updated readme.me and added validations
    
    * Updated readme.me and added validations
    
    * Updated readme.me and added validations
    
    * Updated readme.me and added validations
    
    * Updated readme.me and added validations
    
    * Updated readme.me and added validations
    
    * Updated readme.me and added validations
    
    * Updated readme.md
    
    * Updated readme.md
    
    * Updated readme.md
    
    * Updated readme.md
    
    * Added module to release-please

---
+++ modules/azure-nsg-nsr/main.tf
@@ -0,0 +1,28 @@
+# RESOURCES SECTION
+# https://registry.terraform.io/providers/hashicorp/azurerm/4.16.0/docs/resources/network_security_group
+resource "azurerm_network_security_group" "this" {
+  name                = var.nsg.name
+  location            = var.nsg.location
+  resource_group_name = var.nsg.resource_group_name
+  tags                = var.nsg.tags
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/4.16.0/docs/resources/network_security_rule
+resource "azurerm_network_security_rule" "this" {
+  for_each                     = var.rules
+  name                         = each.value.name
+  priority                     = each.value.priority
+  direction                    = each.value.direction
+  access                       = each.value.access
+  protocol                     = each.value.protocol
+  source_port_range            = each.value.source_port_range
+  source_port_ranges           = each.value.source_port_ranges
+  destination_port_range       = each.value.destination_port_range
+  destination_port_ranges      = each.value.destination_port_ranges
+  source_address_prefix        = each.value.source_address_prefix
+  source_address_prefixes      = each.value.source_address_prefixes
+  destination_address_prefix   = each.value.destination_address_prefix
+  destination_address_prefixes = each.value.destination_address_prefixes
+  resource_group_name          = var.nsg.resource_group_name
+  network_security_group_name  = var.nsg.name
+}
---
+++ modules/azure-nsg-nsr/variables.tf
@@ -0,0 +1,65 @@
+# VARIABLES SECTION
+variable "nsg" {
+  description = "Network Security Group configuration"
+  type = object({
+    name                = string
+    location            = string
+    resource_group_name = string
+    tags                = optional(map(string))
+  })
+}
+
+variable "rules" {
+  description = "Network Security Rule configuration"
+  type = map(object({
+    name                         = string
+    priority                     = number
+    direction                    = string
+    access                       = string
+    protocol                     = string
+    source_port_range            = optional(string)
+    source_port_ranges           = optional(list(string))
+    destination_port_range       = optional(string)
+    destination_port_ranges      = optional(list(string))
+    source_address_prefix        = optional(string)
+    source_address_prefixes      = optional(list(string))
+    destination_address_prefix   = optional(string)
+    destination_address_prefixes = optional(list(string))
+  }))
+  validation {
+    condition = alltrue([
+      for rule in var.rules :
+      (rule.source_port_range == null || rule.source_port_ranges == null)
+      || (rule.source_port_range != null && rule.source_port_ranges == null)
+      || (rule.source_port_range == null && rule.source_port_ranges != null)
+    ])
+    error_message = "Only one of 'source_port_range' or 'source_port_ranges' can be specified for each rule."
+  }
+  validation {
+    condition = alltrue([
+      for rule in var.rules :
+      (rule.destination_port_range == null || rule.destination_port_ranges == null)
+      || (rule.destination_port_range != null && rule.destination_port_ranges == null)
+      || (rule.destination_port_range == null && rule.destination_port_ranges != null)
+    ])
+    error_message = "Only one of 'destination_port_range' or 'destination_port_ranges' can be specified for each rule."
+  }
+  validation {
+    condition = alltrue([
+      for rule in var.rules :
+      (rule.source_address_prefix == null || rule.source_address_prefixes == null)
+      || (rule.source_address_prefix != null && rule.source_address_prefixes == null)
+      || (rule.source_address_prefix == null && rule.source_address_prefixes != null)
+    ])
+    error_message = "Only one of 'source_adress_prefix' or 'source_adress_prefixes' can be specified for each rule."
+  }
+  validation {
+    condition = alltrue([
+      for rule in var.rules :
+      (rule.destination_address_prefix == null || rule.destination_address_prefixes == null)
+      || (rule.destination_address_prefix != null && rule.destination_address_prefixes == null)
+      || (rule.destination_address_prefix == null && rule.destination_address_prefixes != null)
+    ])
+    error_message = "Only one of 'destination_adress_prefix' or 'destination_adress_prefixes' can be specified for each rule."
+  }
+}
