c132251968151e37deee0ba6189df6ea0e6bed54
jcframil <framiljuan@gmail.com>
2025-02-28 20:16:33 +0100

    fix condition policy
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-policies/variables.tf
+++ modules/azure-policies/variables.tf
@@ -14,7 +14,7 @@ variable "policy" {
   })
   validation {
     condition = alltrue([
-      contains(["BuiltIn", "Custom", "NotSpecified", "Static"], var.policy_definition.policy_type),
+      contains(["BuiltIn", "Custom", "NotSpecified", "Static"], var.policy.policy_type),
       contains(["All", "Indexed", "Microsoft.ContainerService.Data", "Microsoft.CustomerLockbox.Data", "Microsoft.DataCatalog.Data", "Microsoft.KeyVault.Data", "Microsoft.Kubernetes.Data", "Microsoft.MachineLearningServices.Data", "Microsoft.Network.Data", "Microsoft.Synapse.Data"], var.policy_definition.mode)
     ])
     error_message = "Invalid value for policy_type or mode."
