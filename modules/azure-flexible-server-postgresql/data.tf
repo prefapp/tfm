3e54295399a90f3c0ca507565062b209bd341b62
JulianGV21 <llulian.gomez@gmail.com>
2025-03-04 10:35:36 +0100

    fix values

--- modules/azure-flexible-server-postgresql/data.tf
+++ modules/azure-flexible-server-postgresql/data.tf
@@ -1,21 +1,21 @@
 # Data section
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/subnet
 data "azurerm_subnet" "subnet" {
-  name                 = var.subnet_name
+  name                 = var.subnet.name
   virtual_network_name = var.vnet_name
   resource_group_name  = var.vnet_resource_group_name
 }
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/private_dns_zone
 data "azurerm_private_dns_zone" "dns_private_zone" {
-  name                = var.private_dns_zone_name
-  resource_group_name = var.private_dns_zone_resource_group_name
+  name                = var.dns_private_zone.name
+  resource_group_name = var.dns_private_zone.resource_group_name
 }
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/key_vault
 data "azurerm_key_vault" "key_vault" {
-  name                = var.key_vault_name
-  resource_group_name = var.key_vault_resource_group_name
+  name                = var.key_vault.name
+  resource_group_name = var.key_vault.resource_group_name
 }
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/key_vault_secrets
