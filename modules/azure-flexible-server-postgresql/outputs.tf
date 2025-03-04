37bfa332a92d137fef1c487f95f7f1a90211c776
JulianGV21 <llulian.gomez@gmail.com>
2025-03-04 12:38:54 +0100

    Added outputs

--- modules/azure-flexible-server-postgresql/outputs.tf
+++ modules/azure-flexible-server-postgresql/outputs.tf
@@ -1,11 +1,3 @@
 output "id" {
     value = azurerm_postgresql_flexible_server.this.id
 }
-
-output "subnet_id" {
-    value = azurerm_subnet.subnet.id
-}
-
-output "dns_private_zone_id" {
-    value = azurerm_dns_private_zone.dns_private_zone.id
-}
