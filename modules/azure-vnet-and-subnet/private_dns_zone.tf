60f41ff8d9794d6cee0bb8fcd3ff219c8450cf62
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2025-03-05 16:44:02 +0100

    Upgrade vnet-and-subnet moduel to add pDNSz, vl and peerings

--- modules/azure-vnet-and-subnet/private_dns_zone.tf
+++ modules/azure-vnet-and-subnet/private_dns_zone.tf
@@ -1,6 +1,5 @@
-# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/private_dns_zone
 resource "azurerm_private_dns_zone" "this" {
-  for_each            = { for idx, zone in var.private_dns_zones : idx => zone }
+  for_each            = { for zone in var.private_dns_zones : zone.name => zone }
   name                = each.value.name
   resource_group_name = var.resource_group_name
   tags                = var.tags_from_rg ? data.azurerm_resource_group.resource_group.tags : var.tags
