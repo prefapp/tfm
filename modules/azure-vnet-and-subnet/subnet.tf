8821a43081824f24c90cc0ed8fbb3a837e5d46eb
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2025-03-05 14:00:07 +0100

    Upgrade vnet-and-subnet moduel to add pDNSz, vl and peerings

--- modules/azure-vnet-and-subnet/subnet.tf
+++ modules/azure-vnet-and-subnet/subnet.tf
@@ -5,7 +5,7 @@ locals {
     for vnet_key, vnet_value in var.virtual_network : [
       # Iterate over each subnet in the current virtual network
       for subnet_key, subnet_value in vnet_value.subnets : {
-        vnet_key = vnet_key
+        vnet_key     = vnet_key
         subnet_key   = subnet_key
         subnet_value = subnet_value
       }
@@ -18,11 +18,11 @@ resource "azurerm_subnet" "subnet" {
   for_each = { for subnet in local.subnets : "${subnet.vnet_key}.${subnet.subnet_key}" => subnet }
 
   name                 = each.value.subnet_key
-  resource_group_name  = azurerm_virtual_network.vnet[each.value.vnet_key].resource_group_name
+  resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet[each.value.vnet_key].name
   address_prefixes     = each.value.subnet_value.address_prefixes
 
-  # Use the lookup function to get the list of delegations from the subnet definition, 
+  # Use the lookup function to get the list of delegations from the subnet definition,
   # and use an empty list if the delegation attribute is not specified
   # It’s important to note that while you can have more than one delegation block in a subnet,
   # all delegations in a subnet must be for the same service. For instance,
@@ -33,24 +33,18 @@ resource "azurerm_subnet" "subnet" {
   service_endpoints                             = lookup(each.value.subnet_value, "service_endpoints", [])
 
   # Dynamic block for creating service delegations
-dynamic "delegation" {
-  for_each = each.value.subnet_value.delegation != null ? each.value.subnet_value.delegation : []
-  content {
-    name = delegation.value.name
-    service_delegation {
-      name    = delegation.value.service_delegation.name
-      actions = delegation.value.service_delegation.actions
+  dynamic "delegation" {
+    for_each = each.value.subnet_value.delegation != null ? each.value.subnet_value.delegation : []
+    content {
+      name = delegation.value.name
+      service_delegation {
+        name    = delegation.value.service_delegation.name
+        actions = delegation.value.service_delegation.actions
+      }
     }
   }
-}
 
   depends_on = [
-    azurerm_virtual_network.vnet
+    azurerm_virtual_network.this
   ]
 }
-
-
-
-
-
-
