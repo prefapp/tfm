20ac3ab37a742f73bbcab2b7fd93610ed2d480d4
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2025-03-06 15:39:42 +0100

    Fix outputs

--- modules/azure-vnet-and-subnet/outputs.tf
+++ modules/azure-vnet-and-subnet/outputs.tf
@@ -7,3 +7,18 @@ output "vnet_id" {
 output "subnet_ids" {
   value = { for name, subnet in azurerm_subnet.subnet : name => subnet.id }
 }
+
+# Output the IDs of the private DNS zones
+output "private_dns_zone_ids" {
+  value = [for zone in azurerm_private_dns_zone.this : zone.id]
+}
+
+# Output the IDs of the private DNS zone virtual network links
+output "private_dns_zone_virtual_network_link_ids" {
+  value = [for link in azurerm_private_dns_zone_virtual_network_link.this : link.id]
+}
+
+# Output the IDs of the virtual network peerings with their names as keys
+output "vnet_peering_ids" {
+  value = { for name, peering in azurerm_virtual_network_peering.central-predev : name => peering.id }
+}
