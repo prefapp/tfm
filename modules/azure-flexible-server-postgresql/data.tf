6f995cb7a0c9887382e78c22b8b775bec6595a41
JulianGV21 <llulian.gomez@gmail.com>
2025-03-06 10:15:49 +0100

    testing values

--- modules/azure-flexible-server-postgresql/data.tf
+++ modules/azure-flexible-server-postgresql/data.tf
@@ -14,8 +14,9 @@ data "azurerm_resource_group" "resource_group" {
 data "azurerm_resources" "subnet" {
   type = "Microsoft.Network/virtualNetworks"
   required_tags = {
-    tenant = "${data.azurerm_resource_group.resource_group.tags.tenant}"
-    env    = "${data.azurerm_resource_group.resource_group.tags.env == "pro" ? "predev" : data.azurerm_resource_group.resource_group.tags.env}"
+    application = "common"
+    tenant      = "${data.azurerm_resource_group.resource_group.tags.tenant}"
+    env         = "${data.azurerm_resource_group.resource_group.tags.env == "pro" ? "predev" : data.azurerm_resource_group.resource_group.tags.env}"
   }
 }
 
