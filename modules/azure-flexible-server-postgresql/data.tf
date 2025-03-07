4db0922f5ae3942c4e038f5d0f2f30e8dd1ab2fd
JulianGV21 <llulian.gomez@gmail.com>
2025-03-07 15:31:02 +0100

    dns private zone from tags option added

--- modules/azure-flexible-server-postgresql/data.tf
+++ modules/azure-flexible-server-postgresql/data.tf
@@ -1,12 +1,12 @@
 # Locals section
 locals {
+  tags = var.tags_from_rg ? merge(data.azurerm_resource_group.resource_group.tags, var.tags) : var.tags
   vnet_from_data = can(data.azurerm_resources.vnet[0].resources) ? data.azurerm_resources.vnet[0].resources[0].name : null
   resource_group_from_data = can(data.azurerm_resources.vnet[0].resources) ? data.azurerm_resources.vnet[0].resources[0].resource_group_name : null
   dns_private_zone_from_data = can(data.azurerm_resources.dns_private_zone[0].resources) ? data.azurerm_resources.dns_private_zone[0].resources[0].name : null
-  tags = var.tags_from_rg ? merge(data.azurerm_resource_group.resource_group.tags, var.tags) : var.tags
   virtual_network_name = coalesce(var.vnet.name, local.vnet_from_data)
   resource_group_name  = coalesce(var.vnet.resource_group_name, local.resource_group_from_data)
-  dns_private_zone_name = coalesce(var.dns_private_zone.namen, local.dns_private_zone_from_data)
+  dns_private_zone_name = coalesce(var.dns_private_zone.name, local.dns_private_zone_from_data)
 }
 
 # Data section
@@ -38,6 +38,7 @@ data "azurerm_subnet" "subnet" {
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/private_dns_zone
 data "azurerm_private_dns_zone" "dns_private_zone" {
+  count               = var.dns_private_zone.name != null && var.dns_private_zone.resource_group != "" ? 1 : 0
   name                = local.dns_private_zone_name
   resource_group_name = local.resource_group_name
 }
