790c8b370bca716dc3955594eb832f00779d8380
Jose Alvarez <47305925+jalvarezit@users.noreply.github.com>
2025-03-07 18:56:01 +0100

    Add outputs

--- modules/azure-resource-group/outputs.tf
+++ modules/azure-resource-group/outputs.tf
@@ -1,3 +1,15 @@
 output "resource_group_id" {
   value = azurerm_resource_group.resorce_group.id
 }
+
+output "resource_group_name" {
+  value = azurerm_resource_group.resorce_group.name
+}
+
+output "resource_group_tags" {
+  value = azurerm_resource_group.rg.tags
+}
+
+output "resource_group_location" {
+  value = azurerm_resource_group.rg.location
+}
