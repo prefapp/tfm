fbd107941d44625c1b37cfbac8be0e215e5ef304
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2025-03-10 10:26:58 +0100

    Change dependencie pdnszlink

--- modules/azure-vnet-and-subnet/private_dns_zone_vnet_link.tf
+++ modules/azure-vnet-and-subnet/private_dns_zone_vnet_link.tf
@@ -3,7 +3,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "this" {
   for_each              = var.private_dns_zone_virtual_network_links
   name                  = each.value.name
   resource_group_name   = var.resource_group_name
-  private_dns_zone_name = eazurerm_private_dns_zone[each.key].name
+  private_dns_zone_name = azurerm_private_dns_zone[each.key].name
   virtual_network_id    = azurerm_virtual_network.this.id
   registration_enabled  = each.value.registration_enabled
   tags                  = var.tags_from_rg ? merge(data.azurerm_resource_group.resource_group.tags, var.tags) : var.tags
