63105ad896a725cd355296af1086bedf9403c353
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2025-03-10 10:35:32 +0100

    Change dependencie pdnszlink

--- modules/azure-vnet-and-subnet/private_dns_zone_vnet_link.tf
+++ modules/azure-vnet-and-subnet/private_dns_zone_vnet_link.tf
@@ -1,10 +1,10 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/private_dns_zone_virtual_network_link
 resource "azurerm_private_dns_zone_virtual_network_link" "this" {
-  for_each              = var.private_dns_zone_virtual_network_links
+  for_each              = { for zone in var.private_dns_zones : zone.name => zone }
   name                  = each.value.name
   resource_group_name   = var.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.this[each.value.private_dns_zone_name].name
+  private_dns_zone_name = each.value.name
   virtual_network_id    = azurerm_virtual_network.this.id
-  registration_enabled  = each.value.registration_enabled
+  registration_enabled  = true
   tags                  = var.tags_from_rg ? merge(data.azurerm_resource_group.resource_group.tags, var.tags) : var.tags
 }
