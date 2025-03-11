3521b911c0883b589701469764a5819ae72bfbe3
jcframil <framiljuan@gmail.com>
2025-03-12 00:58:53 +0100

    check builtin
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-policies/outputs.tf
+++ modules/azure-policies/outputs.tf
@@ -9,17 +9,17 @@ output "policy_definition_names" {
   description = "List of all Azure Policy definition names"
 }
 
-output "resource_policy_assignment_ids" {
-  value       = [for assignment in azurerm_resource_policy_assignment.this : assignment.id]
-  description = "List of all Azure resource policy assignment IDs"
-}
+# output "resource_policy_assignment_ids" {
+#   value       = [for assignment in azurerm_resource_policy_assignment.this : assignment.id]
+#   description = "List of all Azure resource policy assignment IDs"
+# }
 
-output "resource_group_policy_assignment_ids" {
-  value       = [for assignment in azurerm_resource_group_policy_assignment.this : assignment.id]
-  description = "List of all Azure resource group policy assignment IDs"
-}
+# output "resource_group_policy_assignment_ids" {
+#   value       = [for assignment in azurerm_resource_group_policy_assignment.this : assignment.id]
+#   description = "List of all Azure resource group policy assignment IDs"
+# }
 
-output "subscription_policy_assignment_ids" {
-  value       = [for assignment in azurerm_subscription_policy_assignment.this : assignment.id]
-  description = "List of all Azure subscription policy assignment IDs"
-}
+# output "subscription_policy_assignment_ids" {
+#   value       = [for assignment in azurerm_subscription_policy_assignment.this : assignment.id]
+#   description = "List of all Azure subscription policy assignment IDs"
+# }
