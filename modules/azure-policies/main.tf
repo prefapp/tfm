9347b06fe9cbcfb60a75e29b735e90c2b37d94ad
jcframil <framiljuan@gmail.com>
2025-03-11 23:35:17 +0100

    check builtin
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-policies/main.tf
+++ modules/azure-policies/main.tf
@@ -36,7 +36,7 @@ resource "azurerm_policy_definition" "this" {
 resource "azurerm_resource_policy_assignment" "this" {
   for_each             = { for i, assignment in var.assignments : i => assignment if assignment.scope == "resource" }
   name                 = each.value.name
-  policy_definition_id = lookup(each.value, "policy_definition_id", null) != null ? each.value.policy_definition_id : lookup(data.azurerm_policy_definition.this, each.key, null) != null ? data.azurerm_policy_definition.this[each.key].id : null
+  policy_definition_id = lookup(each.value, "policy_definition_id", null) != null ? each.value.policy_definition_id : each.value.policy_type == "builtin" ? lookup(data.azurerm_policy_definition_built_in.this, each.key, null) != null ? data.azurerm_policy_definition_built_in.this[each.key].id : null : lookup(data.azurerm_policy_definition.this, each.key, null) != null ? data.azurerm_policy_definition.this[each.key].id : null
   resource_id          = each.value.resource_id
   description          = each.value.description
   display_name         = each.value.display_name
@@ -90,7 +90,7 @@ resource "azurerm_resource_policy_assignment" "this" {
 resource "azurerm_resource_group_policy_assignment" "this" {
   for_each             = { for i, assignment in var.assignments : i => assignment if assignment.scope == "resource group" }
   name                 = each.value.name
-  policy_definition_id = lookup(each.value, "policy_definition_id", null) != null ? each.value.policy_definition_id : lookup(data.azurerm_policy_definition.this, each.key, null) != null ? data.azurerm_policy_definition.this[each.key].id : null
+  policy_definition_id = lookup(each.value, "policy_definition_id", null) != null ? each.value.policy_definition_id : each.value.policy_type == "builtin" ? lookup(data.azurerm_policy_definition_built_in.this, each.key, null) != null ? data.azurerm_policy_definition_built_in.this[each.key].id : null : lookup(data.azurerm_policy_definition.this, each.key, null) != null ? data.azurerm_policy_definition.this[each.key].id : null
   resource_group_id    = each.value.resource_id
   description          = each.value.description
   display_name         = each.value.display_name
@@ -144,7 +144,7 @@ resource "azurerm_resource_group_policy_assignment" "this" {
 resource "azurerm_subscription_policy_assignment" "this" {
   for_each             = { for i, assignment in var.assignments : i => assignment if assignment.scope == "subscription" }
   name                 = each.value.name
-  policy_definition_id = lookup(each.value, "policy_definition_id", null) != null ? each.value.policy_definition_id : lookup(data.azurerm_policy_definition.this, each.key, null) != null ? data.azurerm_policy_definition.this[each.key].id : null
+  policy_definition_id = lookup(each.value, "policy_definition_id", null) != null ? each.value.policy_definition_id : each.value.policy_type == "builtin" ? lookup(data.azurerm_policy_definition_built_in.this, each.key, null) != null ? data.azurerm_policy_definition_built_in.this[each.key].id : null : lookup(data.azurerm_policy_definition.this, each.key, null) != null ? data.azurerm_policy_definition.this[each.key].id : null
   subscription_id      = data.azurerm_subscription.current.id
   description          = each.value.description
   display_name         = each.value.display_name
@@ -198,7 +198,7 @@ resource "azurerm_subscription_policy_assignment" "this" {
 resource "azurerm_management_group_policy_assignment" "this" {
   for_each             = { for i, assignment in var.assignments : i => assignment if assignment.scope == "management group" }
   name                 = each.value.name
-  policy_definition_id = lookup(each.value, "policy_definition_id", null) != null ? each.value.policy_definition_id : lookup(data.azurerm_policy_definition.this, each.key, null) != null ? data.azurerm_policy_definition.this[each.key].id : null
+  policy_definition_id = lookup(each.value, "policy_definition_id", null) != null ? each.value.policy_definition_id : each.value.policy_type == "builtin" ? lookup(data.azurerm_policy_definition_built_in.this, each.key, null) != null ? data.azurerm_policy_definition_built_in.this[each.key].id : null : lookup(data.azurerm_policy_definition.this, each.key, null) != null ? data.azurerm_policy_definition.this[each.key].id : null
   management_group_id  = each.value.management_group_id
   description          = each.value.description
   display_name         = each.value.display_name
