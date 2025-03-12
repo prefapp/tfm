7e149801ed4ab01657f06b940f140966c09b4c93
JulianGV21 <llulian.gomez@gmail.com>
2025-03-12 16:18:11 +0100

    testing values

--- modules/azure-flexible-server-postgresql/data.tf
+++ modules/azure-flexible-server-postgresql/data.tf
@@ -7,6 +7,8 @@ locals {
   key_vault_resource_group_from_data = can(data.azurerm_resources.key_vault[0].resources) ? data.azurerm_resources.vnet[0].key_vault[0].resource_group_name : null
   resource_group_name  = var.postgresql_flexible_server.public_network_access_enabled == false ? try(coalesce(var.vnet.resource_group_name, local.vnet_resource_group_from_data), null) : null
   virtual_network_name = var.postgresql_flexible_server.public_network_access_enabled == false ? try(coalesce(var.vnet.name, local.vnet_from_data), null) : null
+  key_vault_resource_group_name  = try(coalesce(var.key_vault.resource_group_name, local.key_vault_resource_group_from_data), null) : null
+  key_vault_name = try(coalesce(var.key_vault.name, local.key_vault_from_data), null) : null
 }
 
 # Data section
@@ -48,8 +50,8 @@ data "azurerm_private_dns_zone" "dns_private_zone" {
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/key_vault
 data "azurerm_key_vault" "key_vault" {
   count               = var.key_vault != null && var.key_vault.name != "" && var.key_vault.resource_group_name != "" ? 1 : 0
-  name                = try(coalesce(var.key_vault.name, local.key_vault_from_data), null)
-  resource_group_name = try(coalesce(var.key_vault.resource_group_name, local.key_vault_resource_group_from_data), null)
+  name                = local.key_vault_name
+  resource_group_name = local.key_vault_resource_group_name
 }
 
 data "azurerm_resources" "key_vault" {
