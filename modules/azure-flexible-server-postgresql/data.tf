3307feda60d3eecd6a480a830d12aba3e6050443
JulianGV21 <llulian.gomez@gmail.com>
2025-03-12 17:52:02 +0100

    testing values

--- modules/azure-flexible-server-postgresql/data.tf
+++ modules/azure-flexible-server-postgresql/data.tf
@@ -64,5 +64,5 @@ data "azurerm_resources" "key_vault" {
 data "azurerm_key_vault_secret" "administrator_password" {
   count        = var.administrator_password_key_vault_secret_name != null && var.administrator_password_key_vault_secret_name != "" ? 1 : 0
   name         = var.administrator_password_key_vault_secret_name
-  key_vault_id = key_vault_id = try(data.azurerm_key_vault.key_vault[0].id, try(data.azurerm_resources.key_vault[0].resources[0].id, null))
+  key_vault_id = try(data.azurerm_key_vault.key_vault[0].id, try(data.azurerm_resources.key_vault[0].resources[0].id, null))
 }
