ad9e1ce124ff930284413e76051beaa55c04df23
jcframil <framiljuan@gmail.com>
2025-03-12 15:44:12 +0100

    management group data
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-policies-assignment/main.tf
+++ modules/azure-policies-assignment/main.tf
@@ -4,7 +4,7 @@ data "azurerm_subscription" "current" {}
 
 data "azurerm_management_group" "this" {
   for_each = { for k, v in var.assignments : k => v if can(v.management_group_name) && v.scope == "management group" }
-  name = each.value.management_group_name
+  display_name = each.value.management_group_name
 }
 
 data "azurerm_policy_definition" "this" {
