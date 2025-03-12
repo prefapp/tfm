4bfe395c832f2f0cd39872431865174c19f7c3aa
JulianGV21 <llulian.gomez@gmail.com>
2025-03-12 16:09:26 +0100

    testing values

--- modules/azure-flexible-server-postgresql/data.tf
+++ modules/azure-flexible-server-postgresql/data.tf
@@ -2,8 +2,10 @@
 locals {
   tags = var.tags_from_rg ? merge(data.azurerm_resource_group.resource_group.tags, var.tags) : var.tags
   vnet_from_data = can(data.azurerm_resources.vnet[0].resources) ? data.azurerm_resources.vnet[0].resources[0].name : null
-  resource_group_from_data = can(data.azurerm_resources.vnet[0].resources) ? data.azurerm_resources.vnet[0].resources[0].resource_group_name : null
-  resource_group_name  = var.postgresql_flexible_server.public_network_access_enabled == false ? try(coalesce(var.vnet.resource_group_name, local.resource_group_from_data), null) : null
+  vnet_resource_group_from_data = can(data.azurerm_resources.vnet[0].resources) ? data.azurerm_resources.vnet[0].resources[0].resource_group_name : null
+  key_vault_from_data = can(data.azurerm_resources.key_vault[0].resources) ? data.azurerm_resources.key_vault[0].resources[0].name : null
+  key_vault_resource_group_from_data = can(data.azurerm_resources.key_vault[0].resources) ? data.azurerm_resources.vnet[0].key_vault[0].resource_group_name : null
+  resource_group_name  = var.postgresql_flexible_server.public_network_access_enabled == false ? try(coalesce(var.vnet.resource_group_name, local.vnet_resource_group_from_data), null) : null
   virtual_network_name = var.postgresql_flexible_server.public_network_access_enabled == false ? try(coalesce(var.vnet.name, local.vnet_from_data), null) : null
 }
 
@@ -17,7 +19,7 @@ data "azurerm_resource_group" "resource_group" {
 data "azurerm_virtual_network" "vnet" {
   count               = var.vnet.name != null && var.vnet.resource_group_name != "" ? 1 : 0
   name                = var.vnet.name
-  resource_group_name = local.resource_group_from_data
+  resource_group_name = local.vnet_resource_group_from_data
 }
 
 data "azurerm_resources" "vnet" {
@@ -31,7 +33,7 @@ data "azurerm_subnet" "subnet" {
   count = var.subnet.name != null && var.subnet.name != "" ? 1 : 0
   name                 = var.subnet.name
   virtual_network_name = local.virtual_network_name
-  resource_group_name  = local.resource_group_from_data
+  resource_group_name  = local.vnet_resource_group_from_data
 }
 
 
@@ -39,21 +41,21 @@ data "azurerm_subnet" "subnet" {
 data "azurerm_private_dns_zone" "dns_private_zone" {
   count = var.dns_private_zone_name != null && var.dns_private_zone_name != "" ? 1 : 0
   name                = var.dns_private_zone_name
-  resource_group_name = local.resource_group_from_data
+  resource_group_name = local.vnet_resource_group_from_data
 }
 
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/key_vault
 data "azurerm_key_vault" "key_vault" {
   count               = var.key_vault != null && var.key_vault.name != "" && var.key_vault.resource_group_name != "" ? 1 : 0
-  name                = var.key_vault.name
-  resource_group_name = var.key_vault.resource_group_name
+  name                = coalesce(var.key_vault.name, local.key_vault_from_data)
+  resource_group_name = coalesce(var.key_vault.resource_group_name, local.key_vault_resource_group_from_data)
 }
 
 data "azurerm_resources" "key_vault" {
   count = length(var.key_vault_tags) > 0 ? 1 : 0
   type = "Microsoft.KeyVault"
-  required_tags = var.key_vault_tags.layer
+  required_tags = var.key_vault_tags
 }
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/key_vault_secrets
