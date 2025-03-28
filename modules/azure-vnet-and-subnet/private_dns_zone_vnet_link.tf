88ffdd9cac9cc4d10604c1e2790cf3ad173408f3
Juan CFF <97174951+jcframil@users.noreply.github.com>
2025-03-28 18:44:13 +0100

    fix: coalesce to control optional link_name (#491)
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-vnet-and-subnet/private_dns_zone_vnet_link.tf
+++ modules/azure-vnet-and-subnet/private_dns_zone_vnet_link.tf
@@ -1,7 +1,7 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/private_dns_zone_virtual_network_link
 resource "azurerm_private_dns_zone_virtual_network_link" "this" {
   for_each              = { for zone in var.private_dns_zones : zone.name => zone }
-  name                  = each.value.link_name
+  name                  = coalesce(each.value.link_name, each.value.name)
   resource_group_name   = var.resource_group_name
   private_dns_zone_name = each.value.name
   virtual_network_id    = azurerm_virtual_network.this.id
