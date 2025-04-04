76e8ce726223f9f0914979c758f9ec5be05aa200
jcframil <framiljuan@gmail.com>
2025-04-04 11:04:48 +0200

    upgrade version and fix outputs
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-resource-group/resource_groups.tf
+++ modules/azure-resource-group/resource_groups.tf
@@ -1,5 +1,5 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
-resource "azurerm_resource_group" "resorce_group" {
+resource "azurerm_resource_group" "this" {
   name     = var.name
   location = var.location
   tags     = var.tags != null ? var.tags : {}
