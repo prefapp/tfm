190ad3313e3715e2dd2105c29464c7c560c9da4e
jcframil <framiljuan@gmail.com>
2025-04-04 11:02:03 +0200

    upgrade version and fix outputs
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-resource-group/outputs.tf
+++ modules/azure-resource-group/outputs.tf
@@ -1,15 +1,15 @@
 output "resource_group_id" {
-  value = azurerm_resource_group.resorce_group.id
+  value       = azurerm_resource_group.this.id
 }
 
 output "resource_group_name" {
-  value = azurerm_resource_group.resorce_group.name
-}
-
-output "resource_group_tags" {
-  value = azurerm_resource_group.rg.tags
+  value       = azurerm_resource_group.this.name
 }
 
 output "resource_group_location" {
-  value = azurerm_resource_group.rg.location
+  value       = azurerm_resource_group.this.location
+}
+
+output "resource_group_tags" {
+  value       = azurerm_resource_group.this.tags
 }
