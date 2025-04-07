bb1993d73011495691aef9b0685fd7a0ce02086d
Javier Gomez Rodriguez <javiergr@ext.zara.com>
2025-04-07 11:52:48 +0200

    fix: avoid destroy of resource group previously created

--- modules/azure-resource-group/resource_groups.tf
+++ modules/azure-resource-group/resource_groups.tf
@@ -4,3 +4,8 @@ resource "azurerm_resource_group" "this" {
   location = var.location
   tags     = var.tags != null ? var.tags : {}
 }
+
+moved {
+  from = azurerm_resource_group.resorce_group
+  to   = azurerm_resource_group.this
+}
--- modules/azure-resource-group/variables.tf
+++ modules/azure-resource-group/variables.tf
@@ -1,20 +1,20 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group#name
 variable "name" {
-  type    = string
-  default = ""
+  type        = string
+  default     = ""
   description = "(Required) The Name which should be used for this Resource Group. Changing this forces a new Resource Group to be created."
 }
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group#location
 variable "location" {
-  type    = string
-  default = ""
+  type        = string
+  default     = ""
   description = "(Required) The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."
 }
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group#tags
 variable "tags" {
-  type    = map
-  default = {}
+  type        = map(any)
+  default     = {}
   description = "(Optional) A mapping of tags which should be assigned to the Resource Group."
 }
