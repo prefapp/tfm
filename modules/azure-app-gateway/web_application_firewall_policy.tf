d06008e2cea8fe945f790dfe81779aad87419856
Juan CFF <97174951+jcframil@users.noreply.github.com>
2025-04-11 13:14:53 +0200

    feat: add custom rules azure app gateway (#500)
    * add custom rules azure app gateway
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * add custom rules azure app gateway
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * add custom rules azure app gateway
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * add custom rules azure app gateway
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    ---------
    
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-app-gateway/web_application_firewall_policy.tf
+++ modules/azure-app-gateway/web_application_firewall_policy.tf
@@ -3,7 +3,7 @@ resource "azurerm_web_application_firewall_policy" "default_waf_policy" {
   name                = var.web_application_firewall_policy.name #"central-waf-policy-default-predev"
   resource_group_name = var.resource_group_name
   location            = var.location
-  # Max 1 managed_rule_set block
+
   policy_settings {
     enabled = var.web_application_firewall_policy.policy_settings.enabled
     mode    = var.web_application_firewall_policy.policy_settings.mode
@@ -11,6 +11,40 @@ resource "azurerm_web_application_firewall_policy" "default_waf_policy" {
     request_body_enforcement = var.web_application_firewall_policy.policy_settings.request_body_enforcement
     file_upload_limit_in_mb = var.web_application_firewall_policy.policy_settings.file_upload_limit_in_mb
     max_request_body_size_in_kb = var.web_application_firewall_policy.policy_settings.max_request_body_size_in_kb
+  }
+
+  dynamic "custom_rules" {
+    for_each = var.web_application_firewall_policy.custom_rules
+    content {
+      enabled    = coalesce(custom_rules.value.enabled, true)
+      name       = custom_rules.value.name
+      priority   = custom_rules.value.priority
+      rule_type  = custom_rules.value.rule_type
+      action     = custom_rules.value.action
+
+      # Optional fields for RateLimitRule
+      rate_limit_duration   = custom_rules.value.rate_limit_duration
+      rate_limit_threshold  = custom_rules.value.rate_limit_threshold
+      group_rate_limit_by   = custom_rules.value.group_rate_limit_by
+
+      dynamic "match_conditions" {
+        for_each = coalesce(custom_rules.value.match_conditions, [])
+        content {
+          operator           = match_conditions.value.operator
+          negation_condition = coalesce(match_conditions.value.negation_condition, false)
+          match_values       = match_conditions.value.match_values
+          transforms         = match_conditions.value.transforms
+
+          dynamic "match_variables" {
+            for_each = coalesce(match_conditions.value.match_variables, [])
+            content {
+              variable_name = match_variables.value.variable_name
+              selector      = match_variables.value.selector
+            }
+          }
+        }
+      }
+    }
   }
 
   managed_rules {
