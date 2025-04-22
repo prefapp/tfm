90e682d4a7055652620ea6a890a675c31bd03c68
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2025-04-22 09:51:30 +0200

    Change origin id

--- modules/cloudamqp-cluster/variables.tf
+++ modules/cloudamqp-cluster/variables.tf
@@ -8,7 +8,7 @@ variable "cloudamqp_instance" {
     tags                = optional(list(string), [])
     nodes               = number
     rmq_version         = string
-    vpc_id              = optional(string)
+    vpc_id              = optional(number)
     no_default_alarms   = optional(bool)
     keep_associated_vpc = optional(bool)
   })
