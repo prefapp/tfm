0514f6f3d42f3cbd44adadcdf4228fc5c73e6701
Gustavo Esteban Borragán <68920322+gustavoborragan@users.noreply.github.com>
2025-04-22 10:31:41 +0200

    fix!: Remove vpc connect into cloudamqp-vpc module (#506)

--- modules/cloudamqp-vpc/variables.tf
+++ modules/cloudamqp-vpc/variables.tf
@@ -7,14 +7,3 @@ variable "cloudamqp_vpc" {
     tags   = optional(list(string))
   })
 }
-
-variable "cloudamqp_vpc_connect" {
-  description = "CloudAMQP vpc_connect configurations"
-  type = object({
-    instance_id            = number
-    region                 = string
-    approved_subscriptions = optional(list(string))
-    sleep                  = optional(number)
-    timeout                = optional(number)
-  })
-}
