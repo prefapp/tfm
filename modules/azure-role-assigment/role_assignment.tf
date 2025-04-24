8e5e9f0bdcf9bba75da0aa27c43bc8eeb2383337
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2025-04-24 11:26:26 +0200

    Add azure-role-assignment module

---
+++ modules/azure-role-assigment/role_assignment.tf
@@ -0,0 +1,8 @@
+# https://registry.terraform.io/providers/hashicorp/azurerm/4.26.0/docs/resources/role_assignment
+resource "azurerm_role_assignment" "role_assignment" {
+  for_each             = var.role_assignments
+  scope                = each.key
+  principal_id         = each.value.target_id
+  role_definition_name = contains(keys(each.value), "role_definition_name") ? each.value.role_definition_name : null
+  role_definition_id   = contains(keys(each.value), "role_definition_id") ? each.value.role_definition_id : null
+}
