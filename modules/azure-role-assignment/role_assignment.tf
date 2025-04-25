29f774a7b299d288eb7da0315a89852ce108337a
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2025-04-25 09:47:15 +0200

    Fix terraform version azure-role-assignment module

--- modules/azure-role-assignment/role_assignment.tf
+++ modules/azure-role-assignment/role_assignment.tf
@@ -2,7 +2,7 @@
 resource "azurerm_role_assignment" "role_assignment" {
   for_each             = var.role_assignments
   scope                = each.value.scope
-  principal_id         = each.value.target_id
+  delegated_managed_identity_resource_id         = each.value.target_id
   principal_type       = each.value.type
   role_definition_name = contains(keys(each.value), "role_definition_name") ? each.value.role_definition_name : null
   role_definition_id   = contains(keys(each.value), "role_definition_id") ? each.value.role_definition_id : null
