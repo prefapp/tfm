caa66d803857292b9f2d389ef610aa57aef536bf
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2025-04-25 08:33:32 +0200

    Fix terraform version azure-role-assignment module

--- modules/azure-role-assignment/variables.tf
+++ modules/azure-role-assignment/variables.tf
@@ -3,9 +3,10 @@ variable "role_assignments" {
   description = "A map of role assignments to create. The key is the scope, and the value is a map containing the role definition name and target ID."
   type = map(object({
     scope                = string
+    target_id            = string
+    type                 = optional(string, "ServicePrincipal")
     role_definition_name = optional(string)
     role_definition_id   = optional(string)
-    target_id            = string
   }))
 
   validation {
@@ -16,5 +17,15 @@ variable "role_assignments" {
     ])
     error_message = "Each role assignment must have either 'role_definition_name' or 'role_definition_id', but not both."
   }
+
+  validation {
+    condition = alltrue([
+      for k, v in var.role_assignments : (
+        v.type == "ServicePrincipal" || v.type == "User" || v.type == "Group"
+      )
+    ])
+    error_message = "The 'type' field must be one of 'ServicePrincipal', 'User', or 'Group'."
+  }
+
   default = {}
 }
