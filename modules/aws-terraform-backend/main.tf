57aea324a8b3f189b4b8a805b9dc7ab418203a37
Félix González <felix.gonzalez@prefapp.es>
2025-05-05 18:02:18 +0200

    Revert main.tf to use count

--- modules/aws-terraform-backend/main.tf
+++ modules/aws-terraform-backend/main.tf
@@ -34,10 +34,9 @@ resource "aws_s3_bucket_public_access_block" "this" {
 
 # Only create DynamoDB table if name is provided
 resource "aws_dynamodb_table" "this" {
-  # count = var.dynamodb_table_name == "" ? 0 : 1
-  for_each = var.dynamodb_table_name == "" ? {} : { this = var.dynamodb_table_name }
+  count = var.dynamodb_table_name == "" ? 0 : 1
 
-  name         = each.value
+  name         = var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
