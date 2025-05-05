72ee09bcffdf09f2e24a45b41117849d9e799834
Félix González <felix.gonzalez@prefapp.es>
2025-05-05 16:48:31 +0200

    Create tables with loop

--- modules/aws-terraform-backend/main.tf
+++ modules/aws-terraform-backend/main.tf
@@ -34,7 +34,9 @@ resource "aws_s3_bucket_public_access_block" "this" {
 
 # Only create DynamoDB table if name is provided
 resource "aws_dynamodb_table" "this" {
-  name         = var.dynamodb_table_name == "" ? null : var.dynamodb_table_name
+  for_each = var.dynamodb_table_name == "" ? {} : { "this" = var.dynamodb_table_name }
+
+  name         = each.value
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
