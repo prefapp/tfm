d621991c2a6190831941d4cdf6e4c37e1e440ccb
Félix González <felix.gonzalez@prefapp.es>
2025-05-05 15:57:49 +0200

    Fix resource name in outputs

--- modules/aws-terraform-backend/outputs.tf
+++ modules/aws-terraform-backend/outputs.tf
@@ -1,11 +1,11 @@
 output "s3_bucket_arn" {
   description = "ARN of the S3 bucket"
-  value       = aws_s3_bucket.terraform_state.arn
+  value       = aws_s3_bucket.this.arn
 }
 
 output "s3_bucket_name" {
   description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.terraform_state.id
+  value       = aws_s3_bucket.this.id
 }
 
 
