fa14f978ae67887788c10e77ee556dd5fc7638f0
Félix González <felix.gonzalez@prefapp.es>
2025-05-05 16:50:12 +0200

    Add index to outputs

--- modules/aws-terraform-backend/outputs.tf
+++ modules/aws-terraform-backend/outputs.tf
@@ -11,10 +11,10 @@ output "s3_bucket_name" {
 
 output "dynamodb_table_arn" {
   description = "ARN of the DynamoDB table (empty if not created)"
-  value       = coalesce(aws_dynamodb_table.this.arn, "")
+  value       = coalesce(aws_dynamodb_table.this[0].arn, "")
 }
 
 output "dynamodb_table_name" {
   description = "Name of the DynamoDB table (empty if not created)"
-  value       = coalesce(aws_dynamodb_table.this.name, "")
+  value       = coalesce(aws_dynamodb_table.this[0].name, "")
 }
