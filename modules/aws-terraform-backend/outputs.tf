8c51ac32045b879c1e8ad380fb0f4598a3b6f8c1
Félix González <felix.gonzalez@prefapp.es>
2025-05-05 17:31:32 +0200

    Remove redundant outputs

--- modules/aws-terraform-backend/outputs.tf
+++ modules/aws-terraform-backend/outputs.tf
@@ -3,18 +3,7 @@ output "s3_bucket_arn" {
   value       = aws_s3_bucket.this.arn
 }
 
-output "s3_bucket_name" {
-  description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.this.id
-}
-
-
-output "dynamodb_table_arn" {
+output "dynamodb_locks_table_arn" {
   description = "ARN of the DynamoDB table (empty if not created)"
   value       = var.dynamodb_table_name == "" ? "" : aws_dynamodb_table.this[0].arn
-}
-
-output "dynamodb_table_name" {
-  description = "Name of the DynamoDB table (empty if not created)"
-  value       = var.dynamodb_table_name == "" ? "" : aws_dynamodb_table.this[0].name
 }
