7f5a86a2cbe88944f0496d615e5f96afdb2c068a
Jose Alvarez <jose.alvarez@prefapp.es>
2025-05-09 10:40:38 +0200

    Add outputs

--- modules/azure-application/outputs.tf
+++ modules/azure-application/outputs.tf
@@ -1,5 +1,11 @@
 ## OUTPUTS SECTION
-# User Assigned Identity
-# output "id" {
-#   value = azurerm_user_assigned_identity.this.id
-# }
+
+output "application_client_id" {
+    description = "The client ID of the Azure application"
+    value       = azuread_application.this.client_id
+}
+
+output "application_object_id" {
+    description = "The object ID of the Azure application"
+    value       = azuread_application.this.object_id
+}
