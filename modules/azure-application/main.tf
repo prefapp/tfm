a618446bad7902fa55588223b29b2e65fc0e1866
Jose Alvarez <jose.alvarez@prefapp.es>
2025-05-09 12:33:48 +0200

    Use secret instead

--- modules/azure-application/main.tf
+++ modules/azure-application/main.tf
@@ -13,7 +13,7 @@ data "azuread_service_principal" "msgraph" {
 
 import {
   id = "/subscriptions/8b17a4df-07f3-4fb1-a704-b92d3fb6334f/resourceGroups/central-management/providers/Microsoft.KeyVault/vaults/central-management-kv/secrets/argo-sso-poc"
-  to = azuread_application_password.this[0]
+  to = azurerm_key_vault_secret.this[0]
 }
 
 # RESOURCES SECTION
