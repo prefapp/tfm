f438f945cbdf3066539ee4d86f66116c312538de
Félix González <felix.gonzalez@prefapp.es>
2025-06-05 09:26:18 +0200

    Restore role

--- modules/aws-terraform-backend/locals.tf
+++ modules/aws-terraform-backend/locals.tf
@@ -1,5 +1,6 @@
 locals {
   rendered_template = templatefile("${path.module}/template.yaml", {
-    FirestartrAccountId = var.aws_account_id
+    FirestartrAccountId   = var.aws_account_id
+    FirestartrAccountRole = var.aws_account_role
   })
 }
