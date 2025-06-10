59101188ac3e3be8c45922f70c7c43891a8b538a
Félix González <felix.gonzalez@prefapp.es>
2025-06-10 12:17:33 +0200

    Apply changes in variable names

--- modules/aws-terraform-backend/locals.tf
+++ modules/aws-terraform-backend/locals.tf
@@ -4,8 +4,8 @@ locals {
 
   # template with variable interpolation
   rendered_template = templatefile("${path.module}/template.yaml", {
-    FirestartrAccountId   = var.aws_account_id
-    FirestartrAccountRole = var.aws_account_role
+    FirestartrAccountId = var.aws_account_id
+    FirestartrRoleName  = var.aws_account_role
   })
 
   # Only create a S3 object if a bucket is specified
