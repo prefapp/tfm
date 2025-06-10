c39edbab35edc42fdbb040d8c2eb2354d8b66e77
JorgeIglesiasMosquera <jimgotch@gmail.com>
2025-06-10 09:33:38 +0200

    wip

--- modules/aws-eks/locals.tf
+++ modules/aws-eks/locals.tf
@@ -1,5 +1,5 @@
-# Get only two of the private subnets to create the cluster
 locals {
+  # Get only two of the private subnets to create the cluster
   node_groups_with_subnets = {
     for group_name, group in var.node_groups : group_name => merge(
       group,
@@ -12,32 +12,24 @@ locals {
       }
     )
   }
-}
 
-locals {
   vpc_tag_filters = var.vpc_tags != null ? [
     for k, v in var.vpc_tags : {
       name   = "tag:${k}"
       values = [v]
     }
   ] : []
-}
 
-
-locals {
   subnet_tag_filters = var.subnet_tags != null ? [
     for k, v in var.subnet_tags : {
       name   = "tag:${k}"
       values = [v]
     }
   ] : []
-}
 
-
-#######################################
-# Final Subnet Selection (Dynamic Logic)
-#######################################
-# Decide which subnet IDs to use (filtered or manually provided)
-locals {
+  #######################################
+  # Final Subnet Selection (Dynamic Logic)
+  #######################################
+  # Decide which subnet IDs to use (filtered or manually provided)
   selected_subnet_ids = var.subnet_tags != null && length(data.aws_subnets.filtered.ids) > 0 ? data.aws_subnets.filtered.ids : var.subnet_ids
 }
