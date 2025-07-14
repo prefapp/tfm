0fc967a50f85f70f7ca7b00fe3f133a031a45d32
jcframil <framiljuan@gmail.com>
2025-07-14 18:33:41 +0200

    change reference to vault in postgresql
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-backup-vault/postgresql.tf
+++ modules/azure-backup-vault/postgresql.tf
@@ -1,7 +1,7 @@
 # Role assignment: Backup Contributor to the vault
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/role_assignment
 resource "azurerm_role_assignment" "vault_backup_contributor_postgresql" {
-  scope                = azurerm_data_protection_backup_vault.postgresql.id
+  scope                = azurerm_data_protection_backup_vault.this.id
   role_definition_name = "Backup Contributor"
   principal_id         = azurerm_data_protection_backup_vault.this.identity[0].principal_id
 }
