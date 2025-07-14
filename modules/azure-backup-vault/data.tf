b195f4fc38367e03326a05ce32e3d5d06b28d3c7
jcframil <framiljuan@gmail.com>
2025-07-14 18:21:56 +0200

    add data source client config
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-backup-vault/data.tf
+++ modules/azure-backup-vault/data.tf
@@ -1,14 +1,19 @@
-#Locals section
+# Locals section
 locals {
   tags = var.tags_from_rg ? merge(data.azurerm_resource_group.resource_group.tags, var.tags) : var.tags
 }
 
-#Data Section
+# Data source to get the current Azure client configuration
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources
+data "azurerm_client_config" "current" {}
+
+# Data source to get the resource group for the backup vault
 #https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
 data "azurerm_resource_group" "resource_group" {
   name = var.backup_resource_group_name
 }
 
+# Data source to get the managed disk by name
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/managed_disk
 data "azurerm_managed_disk" "this" {
   for_each            = { for instance in var.disk_instances : instance.disk_name => instance }
@@ -17,6 +22,7 @@ data "azurerm_managed_disk" "this" {
 }
 
 # Data source to get cluster ID by name
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/kubernetes_cluster
 data "azurerm_kubernetes_cluster" "this" {
   for_each            = var.kubernetes_instances
   name                = each.value.cluster_name
