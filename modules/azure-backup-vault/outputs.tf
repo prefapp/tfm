f7a7a4faf66e2c81215b68a16e0eda5bee508d9f
jcframil <framiljuan@gmail.com>
2025-07-15 10:10:31 +0200

    index for output vault
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-backup-vault/outputs.tf
+++ modules/azure-backup-vault/outputs.tf
@@ -1,3 +1,4 @@
 output "vault_id" {
-  value = azurerm_data_protection_backup_vault.this[0].id
+  value       = length(azurerm_data_protection_backup_vault.this) > 0 ? azurerm_data_protection_backup_vault.this[0].id : null
+  description = "ID of the backup vault (if exists)"
 }
