8bb52737ba6f88a30aec5de9f67f9c48c7dd81da
jcframil <framiljuan@gmail.com>
2025-07-16 17:43:13 +0200

    add role assignments
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-policy-assignments/main.tf
+++ modules/azure-policy-assignments/main.tf
@@ -259,13 +259,16 @@ resource "azurerm_management_group_policy_assignment" "this" {
 
 resource "azurerm_role_assignment" "this" {
   for_each = {
-    for ra in var.policy_identity_role_assignments : "${ra.assignment_name}-${ra.scope}-${ra.role_definition_name}" => ra }
+    for ra in var.policy_identity_role_assignments :
+    "${ra.assignment_name}-${ra.scope}-${ra.role_definition_name}" => ra
+  }
 
-  principal_id = coalesce(
-    try(azurerm_subscription_policy_assignment.this[each.value.assignment_name].identity[0].principal_id, null),
-    try(azurerm_resource_group_policy_assignment.this[each.value.assignment_name].identity[0].principal_id, null),
-    try(azurerm_management_group_policy_assignment.this[each.value.assignment_name].identity[0].principal_id, null),
-    try(azurerm_resource_policy_assignment.this[each.value.assignment_name].identity[0].principal_id, null)
+  principal_id = (
+    each.value.scope_type == "subscription" ? try(azurerm_subscription_policy_assignment.this[each.value.assignment_name].identity[0].principal_id, null) :
+    each.value.scope_type == "resource group" ? try(azurerm_resource_group_policy_assignment.this[each.value.assignment_name].identity[0].principal_id, null) :
+    each.value.scope_type == "management group" ? try(azurerm_management_group_policy_assignment.this[each.value.assignment_name].identity[0].principal_id, null) :
+    each.value.scope_type == "resource" ? try(azurerm_resource_policy_assignment.this[each.value.assignment_name].identity[0].principal_id, null) :
+    null
   )
   role_definition_name = each.value.role_definition_name
   scope                = each.value.scope
