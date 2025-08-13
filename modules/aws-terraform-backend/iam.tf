83efec6b7232cb0746a147f82a1ccd9e9792e9e0
Félix González <felix.gonzalez@prefapp.es>
2025-08-13 18:08:41 +0200

    Refactor iam file

--- modules/aws-terraform-backend/iam.tf
+++ modules/aws-terraform-backend/iam.tf
@@ -1,35 +1,3 @@
-## we will use the official OIDC role module to create the role
-## terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc
-
-module "main_oidc_role" {
-  source      = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version     = "5.60.0"
-  create_role = true
-  role_name   = var.main_role.name
-
-  # Políticas simplificadas y validadas
-  inline_policy_statements = local.combined_policies
-
-  provider_urls                  = try(tolist(var.main_role.oidc_trust_policies.provider_urls), [])
-  oidc_fully_qualified_subjects  = try(tolist(var.main_role.oidc_trust_policies.fully_qualified_subjects), [])
-  oidc_fully_qualified_audiences = try(tolist(var.main_role.oidc_trust_policies.fully_qualified_audiences), [])
-}
-
-module "aux_oidc_role" {
-  count       = var.create_aux_role ? 1 : 0
-  source      = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version     = "5.60.0"
-  create_role = true
-  role_name   = var.aux_role.name
-
-  # Usamos las mismas políticas para consistencia
-  inline_policy_statements = local.combined_policies
-
-  provider_urls                  = try(tolist(var.aux_role.oidc_trust_policies.provider_urls), [])
-  oidc_fully_qualified_subjects  = try(tolist(var.aux_role.oidc_trust_policies.fully_qualified_subjects), [])
-  oidc_fully_qualified_audiences = try(tolist(var.aux_role.oidc_trust_policies.fully_qualified_audiences), [])
-}
-
 locals {
   base_policies = [
     {
@@ -54,7 +22,6 @@ locals {
       Resource = "${aws_s3_bucket.tfstate.arn}/${var.tfstate_object_prefix}.tflock"
     }
   ]
-
   dynamodb_policy = var.locks_table_name != null && var.locks_table_name != "" ? [
     {
       Sid      = "DynamoDBLockTableAccess",
@@ -63,19 +30,46 @@ locals {
       Resource = aws_dynamodb_table.this[0].arn
     }
   ] : []
-
   assume_role_policy = [
     {
       Sid      = "AssumeRoleAccess",
       Effect   = "Allow",
-      Action   = "sts:AssumeRole",
+      Action   = ["sts:AssumeRole"],
       Resource = "arn:aws:iam::${var.aws_client_account_id}:role/${var.main_role.cloudformation_external_account_role}"
     }
   ]
+  combined_policies = {
+    name = "CombinedPolicy"
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = concat(
+        local.base_policies,
+        local.dynamodb_policy,
+        local.assume_role_policy
+      )
+    })
+  }
+}
 
-  combined_policies = concat(
-    local.base_policies,
-    local.dynamodb_policy,
-    local.assume_role_policy
-  )
+module "main_oidc_role" {
+  source                         = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                        = "5.60.0"
+  create_role                    = true
+  role_name                      = var.main_role.name
+  inline_policy_statements       = [local.combined_policies]
+  provider_urls                  = try(tolist(var.main_role.oidc_trust_policies.provider_urls), [])
+  oidc_fully_qualified_subjects  = try(tolist(var.main_role.oidc_trust_policies.fully_qualified_subjects), [])
+  oidc_fully_qualified_audiences = try(tolist(var.main_role.oidc_trust_policies.fully_qualified_audiences), [])
+}
+
+module "aux_oidc_role" {
+  count                          = var.create_aux_role ? 1 : 0
+  source                         = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                        = "5.60.0"
+  create_role                    = true
+  role_name                      = var.aux_role.name
+  inline_policy_statements       = [local.combined_policies]
+  provider_urls                  = try(tolist(var.aux_role.oidc_trust_policies.provider_urls), [])
+  oidc_fully_qualified_subjects  = try(tolist(var.aux_role.oidc_trust_policies.fully_qualified_subjects), [])
+  oidc_fully_qualified_audiences = try(tolist(var.aux_role.oidc_trust_policies.fully_qualified_audiences), [])
 }
