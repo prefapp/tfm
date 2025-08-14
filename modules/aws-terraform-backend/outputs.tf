aafa7283599923d1f2038f2b7900ef77a7e8b395
Félix González <felix.gonzalez@prefapp.es>
2025-08-14 11:22:56 +0200

    Restore role rendering and add role output

--- modules/aws-terraform-backend/outputs.tf
+++ modules/aws-terraform-backend/outputs.tf
@@ -35,5 +35,5 @@ output "main_role_arn" {
 
 output "aux_role_arn" {
   description = "Auxiliary role ARN"
-  value = module.aux_oidc_role.iam_role_arn
+  value = module.aux_oidc_role[0].iam_role_arn
 }
