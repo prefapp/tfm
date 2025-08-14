400d7422fd1b6b9cbd341f0edcf7541ce391b1f5
Félix González <felix.gonzalez@prefapp.es>
2025-08-14 11:07:08 +0200

    Revert iam file

--- modules/aws-terraform-backend/iam.tf
+++ modules/aws-terraform-backend/iam.tf
@@ -51,9 +51,9 @@ locals {
     policy = jsonencode({
       Version = "2012-10-17"
       Statement = concat(
-        length(local.base_policies) > 0 ? local.base_policies : [],
-        length(local.dynamodb_policy) > 0 ? local.dynamodb_policy : [],
-        length(local.assume_role_policy) > 0 ? local.assume_role_policy : []
+        local.base_policies,
+        local.dynamodb_policy,
+        local.assume_role_policy
       )
     })
   }
@@ -62,41 +62,10 @@ locals {
     policy = jsonencode({
       Version = "2012-10-17"
       Statement = concat(
-        length(local.base_policies) > 0 ? local.base_policies : [],
-        length(local.dynamodb_policy) > 0 ? local.dynamodb_policy : [],
-        length(local.assume_role_policy_aux) > 0 ? local.assume_role_policy_aux : []
+        local.base_policies,
+        local.dynamodb_policy,
+        local.assume_role_policy_aux
       )
     })
   }
-}
-
-module "main_oidc_role" {
-  source                         = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version                        = "5.60.0"
-  create_role                    = true
-  role_name                      = var.main_role.name
-  inline_policy_statements       = [local.main_combined_policies]
-  provider_urls                  = try(tolist(var.main_role.oidc_trust_policies.provider_urls), [])
-  oidc_fully_qualified_subjects  = try(tolist(var.main_role.oidc_trust_policies.fully_qualified_subjects), [])
-  oidc_fully_qualified_audiences = try(tolist(var.main_role.oidc_trust_policies.fully_qualified_audiences), [])
-}
-
-module "aux_oidc_role" {
-  count                          = var.create_aux_role ? 1 : 0
-  source                         = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version                        = "5.60.0"
-  create_role                    = true
-  role_name                      = var.aux_role.name
-  inline_policy_statements       = [local.aux_combined_policies]
-  provider_urls                  = try(tolist(var.aux_role.oidc_trust_policies.provider_urls), [])
-  oidc_fully_qualified_subjects  = try(tolist(var.aux_role.oidc_trust_policies.fully_qualified_subjects), [])
-  oidc_fully_qualified_audiences = try(tolist(var.aux_role.oidc_trust_policies.fully_qualified_audiences), [])
-}
-
-# Debug print combined policies
-output "main_combined_policies" {
-  value = local.main_combined_policies
-}
-output "aux_combined_policies" {
-  value = local.aux_combined_policies
 }
