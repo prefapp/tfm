b91096dccee026d609374899bbd0ea76244fddeb
Juan Deus Lorenzo <juan.deus@prefapp.es>
2025-10-24 12:21:13 +0200

    Change certificate file regex to include all files

--- modules/azure-app-gateway/locals_blocks_defaults.tf
+++ modules/azure-app-gateway/locals_blocks_defaults.tf
@@ -6,7 +6,7 @@ locals {
 
   trusted_client_certificates = [
     for ca_dir in "${var.ca_dirs}" :{
-      for cert_file in fileset("${path.module}/${ca_dir}", "*.{pem,cer}") : { # File Regex: *.{pem,cer}" catches any file with at least one character name and whose extension is .pem or .cer
+      for cert_file in fileset("${path.module}/${ca_dir}", "*") : {
         name = "${cert_file}"
         data = filebase64("${path.module}/${ca_dir}/${cert_file}")
         dir = "${ca_dir}"
