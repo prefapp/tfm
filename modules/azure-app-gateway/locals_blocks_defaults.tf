93ae8f6035c722c0f624ceb0fe4492b02446d41c
Juan Deus Lorenzo <juan.deus@prefapp.es>
2025-10-24 12:47:32 +0200

    Refactor trusted_client_certificates mapping syntax

--- modules/azure-app-gateway/locals_blocks_defaults.tf
+++ modules/azure-app-gateway/locals_blocks_defaults.tf
@@ -5,8 +5,8 @@ locals {
   ssl_profiles = var.ssl_profiles
 
   trusted_client_certificates = flatten([
-    for ca_dir in var.ca_dirs : {
-      for cert_file in fileset("${path.module}/${ca_dir}", "*.pem") : {
+    for ca_dir in var.ca_dirs : ca_dir => {
+      for cert_file in fileset("${path.module}/${ca_dir}", "*.pem") : cert_file => {
         name = "${cert_file}"
         data = filebase64("${path.module}/${ca_dir}/${cert_file}")
         dir = "${ca_dir}"
