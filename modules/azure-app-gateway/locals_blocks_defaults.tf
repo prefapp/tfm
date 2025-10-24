2a1a78eef5ccdb739441a44b29f5541cf4b1a2e9
Juan Deus Lorenzo <juan.deus@prefapp.es>
2025-10-24 11:47:32 +0200

    Update locals_blocks_defaults.tf

--- modules/azure-app-gateway/locals_blocks_defaults.tf
+++ modules/azure-app-gateway/locals_blocks_defaults.tf
@@ -5,9 +5,11 @@ locals {
   ssl_profiles = var.ssl_profiles
 
   trusted_client_certificates = [
-    for cert_file in fileset("${path.module}/${var.ca-dir}", ".+\.(pem|cer)") : { # File Regex: ".+\.(pem|cer)" catches any file with at least one character name and whose extension is .pem or .cer
-      name = "${cert_file}"
-      data = filebase64("${path.module}/${var.ca-dir}/${cert_file}")
+    for ca_dir in "${var.ca_dirs}" :{
+      for cert_file in fileset("${path.module}/${ca_dir}", ".+\.(pem|cer)") : { # File Regex: ".+\.(pem|cer)" catches any file with at least one character name and whose extension is .pem or .cer
+        name = "${cert_file}"
+        data = filebase64("${path.module}/${ca-dir}/${cert_file}")
+      }
     }
   ]
 
