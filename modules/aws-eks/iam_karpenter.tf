e97c76427b6d22a3f3d070774dc716f8c530a704
pablo.sanchez <pablo.sanchez@prefapp.es>
2025-12-05 09:16:25 +0100

    feat: remove extra values on iam_karpenter

--- modules/aws-eks/iam_karpenter.tf
+++ modules/aws-eks/iam_karpenter.tf
@@ -11,7 +11,7 @@ resource "aws_iam_policy" "iam_policy_extra_karpenter" {
           "ec2:RunInstances",
           "ec2:DeleteLaunchTemplate"
         ],
-        "Resource" : "*",
+        "Resource" : "*"
         "Sid" : "Karpenter"
 
       },
@@ -25,50 +25,9 @@ resource "aws_iam_policy" "iam_policy_extra_karpenter" {
         "Effect" : "Allow",
         "Resource" : "*",
         "Sid" : "ConditionalEC2Termination"
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "iam:PassRole",
-          "iam:CreateInstanceProfile",
-          "iam:AddRoleToInstanceProfile",
-          "iam:RemoveRoleFromInstanceProfile",
-          "iam:DeleteInstanceProfile",
-          "iam:GetInstanceProfile"
-        ],
-        "Resource" : [
-          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/karpenter-${var.cluster_name}*",
-          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/Karpenter-${var.cluster_name}*",
-          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.cluster_name}*",
-          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:instance-profile/karpenter-${var.cluster_name}*",
-          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:instance-profile/Karpenter-${var.cluster_name}*",
-          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:instance-profile/${var.cluster_name}*"
-        ],
-        "Sid" : "KarpenterIAMInstanceProfile"
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "sqs:GetQueueUrl",
-          "sqs:ReceiveMessage",
-          "sqs:DeleteMessage",
-          "sqs:GetQueueAttributes"
-        ],
-        "Resource" : "arn:aws:sqs:*:${data.aws_caller_identity.current.account_id}:karpenter-${var.cluster_name}",
-        "Sid" : "KarpenterSQSInterruptionQueue"
       }
     ]
   })
 
   tags = var.tags
-}
-
-# Attach the extra policy to the Karpenter controller role
-resource "aws_iam_role_policy_attachment" "karpenter_controller_extra_policy" {
-  count      = var.enable_karpenter ? 1 : 0
-  role       = format("%s-karpenter-role", var.cluster_name)
-  policy_arn = aws_iam_policy.iam_policy_extra_karpenter[0].arn
-
-  # Esperar a que el módulo de Karpenter cree el rol antes de adjuntar la política
-  depends_on = [module.karpenter]
 }
