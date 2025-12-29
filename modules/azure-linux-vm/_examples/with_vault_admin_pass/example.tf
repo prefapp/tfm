94876bc062bd1f8a58c6ddb73678476a619ec7c7
Juan CFF <97174951+jcframil@users.noreply.github.com>
2025-12-29 17:10:36 +0100

    feat: azure-linux-vm (#785)
    * feat: azure-linux-vm
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: custom_data value
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: update module references to common vars
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: update module references to common vars
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: update module references to common vars
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: update module references to common vars
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: update tags
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: update tags
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: update tags
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: update tags
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: update tags
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: update tags
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: update tags
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: remove argument non existant
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: add dependency
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: remove nic vars taken from vm
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: remove vm vars taken from nic
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: control custom-data output and default ip address allocation
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: control custom-data output and default ip address allocation
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: add outputs and ifx nic.name
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: add outputs and ifx nic.name
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * feat: add documentation
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * Update modules/azure-linux-vm/docs/footer.md
    
    Co-authored-by: Juan Ramón Sixto Anello <5945892+jotasixto@users.noreply.github.com>
    
    * fix: files type from hcl to tf
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: files type from hcl to tf
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * Update README.md
    
    * fix: files type from hcl to tf
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: files type from hcl to tf
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    * fix: files type from hcl to tf
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    
    ---------
    
    Signed-off-by: jcframil <framiljuan@gmail.com>
    Co-authored-by: Juan Ramón Sixto Anello <5945892+jotasixto@users.noreply.github.com>

---
+++ modules/azure-linux-vm/_examples/with_vault_admin_pass/example.tf
@@ -0,0 +1,41 @@
+# Example: With Vault Admin Password
+
+module "linux_vm" {
+  source = "../../"
+
+  common = {
+    resource_group_name = "vaultpass-rg"
+    location            = "westeurope"
+  }
+  admin_password = {
+    key_vault_name      = "vaultpass-vault"
+    resource_group_name = "vaultpass-vault-rg"
+    secret_name         = "vaultpass-vm-admin-password"
+  }
+  vm = {
+    name       = "vaultpass-vm"
+    size       = "Standard_B2s"
+    admin_username = "azureuser"
+    source_image_reference = {
+      publisher = "Canonical"
+      offer     = "0001-com-ubuntu-server-jammy"
+      sku       = "server"
+      version   = "latest"
+    }
+    os_disk = {
+      caching              = "ReadWrite"
+      disk_size_gb         = 64
+      storage_account_type = "Standard_LRS"
+    }
+    admin_ssh_key = {
+      username   = "azureuser"
+      public_key = "<your-ssh-public-key>"
+    }
+  }
+  nic = {
+    name                = "vaultpass-vm-nic"
+    subnet_name         = "default"
+    virtual_network_name = "vaultpass-vnet"
+    virtual_network_resource_group_name = "vaultpass-rg"
+  }
+}
