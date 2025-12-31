0f4f4dffb285812a960c925f231d6de376749ed5
Rubén Castrelo Suarez <kastras@gmail.com>
2025-12-31 13:35:55 +0100

    feat(aws-ecs): set prefix to avoid naming conflicts

--- modules/aws-ecs/iam.tf
+++ modules/aws-ecs/iam.tf
@@ -1,10 +1,11 @@
 resource "aws_iam_role" "this" {
-  name               = var.iam_role_name
+  count              = var.service_name != null ? 1 : 0
+  name_prefix        = var.iam_role_name
   assume_role_policy = local.ecs_assume_role_policy
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
-  for_each   = toset(var.policy_arns)
-  role       = aws_iam_role.this.name
+  for_each   = var.service_name != null ? toset(var.policy_arns) : []
+  role       = aws_iam_role.this[0].name
   policy_arn = each.value
 }
