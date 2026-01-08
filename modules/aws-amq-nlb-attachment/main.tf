7e2cd7e594e6268179d473b5bf9c1c45c117da4e
Félix González <felix.gonzalez@prefapp.es>
2026-01-08 13:09:28 +0100

    Removed redundant variable definition

--- modules/aws-amq-nlb-attachment/main.tf
+++ modules/aws-amq-nlb-attachment/main.tf
@@ -1,14 +1,3 @@
-# Associates Amazon MQ broker ENIs to an NLB target group
-
-variable "broker_private_ips" {
-  description = "List of private IPs of the broker instances"
-  type        = list(string)
-}
-
-variable "target_group_arn" {
-  description = "ARN of the NLB target group"
-  type        = string
-}
 
 resource "aws_lb_target_group_attachment" "this" {
   for_each = toset(var.broker_private_ips)
