78b00c66042c09a04bb6d4b2eaab671bc3af20b5
juan-dl <juan.deus@prefapp.es>
2026-01-09 09:04:52 +0100

    fix: Add new variables to linux VM module

--- modules/azure-linux-vm/main.tf
+++ modules/azure-linux-vm/main.tf
@@ -1,28 +1,3 @@
-# DATA SECTION
-data "azurerm_subnet" "this" {
-  count = var.nic.subnet_name != null && var.nic.virtual_network_name != null && var.nic.virtual_network_resource_group_name != null ? 1 : 0
-  name                 = var.nic.subnet_name
-  virtual_network_name = var.nic.virtual_network_name
-  resource_group_name  = var.nic.virtual_network_resource_group_name
-}
-
-# https://registry.terraform.io/providers/hashicorp/azurerm/3.91.0/docs/data-sources/resource_group
-data "azurerm_resource_group" "this" {
-  name = var.common.resource_group_name
-}
-
-data "azurerm_key_vault" "this" {
-  count               = var.admin_password != null ? 1 : 0
-  name                = try(var.admin_password.key_vault_name, null)
-  resource_group_name = try(var.admin_password.resource_group_name, null)
-}
-
-data "azurerm_key_vault_secret" "this" {
-  count      = var.admin_password != null ? 1 : 0
-  name       = try(var.admin_password.secret_name, null)
-  key_vault_id = try(data.azurerm_key_vault.this[0].id, null)
-}
-
 # RESOURCES SECTION
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine
 resource "azurerm_linux_virtual_machine" "this" {
@@ -37,9 +12,14 @@ resource "azurerm_linux_virtual_machine" "this" {
   encryption_at_host_enabled      = var.vm.encryption_at_host_enabled
   secure_boot_enabled             = var.vm.secure_boot_enabled
   vtpm_enabled                    = var.vm.vtpm_enabled
+  os_managed_disk_id              = var.vm.os_managed_disk_id
   disable_password_authentication = var.vm.disable_password_authentication
   custom_data                     = var.vm.custom_data != null ? base64encode(var.vm.custom_data) : null
+  provision_vm_agent              = var.vm.provision_vm_agent
+  license_type                    = var.vm.license_type != null ? var.vm.license_type : null
+  patch_mode                      = var.vm.patch_mode != null ? var.vm.patch_mode : null
   tags                            = local.tags
+
   network_interface_ids = [
     azurerm_network_interface.this.id,
   ]
@@ -52,11 +32,13 @@ resource "azurerm_linux_virtual_machine" "this" {
     }
   }
 
-  source_image_reference {
-    publisher = var.vm.source_image_reference.publisher
-    offer     = var.vm.source_image_reference.offer
-    sku       = var.vm.source_image_reference.sku
-    version   = var.vm.source_image_reference.version
+  dynamic "source_image_reference" {
+    for_each = var.vm.source_image_reference != null ? [var.vm.source_image_reference] : []
+    content {
+      publisher = source_image_reference.value.publisher
+      offer     = source_image_reference.value.offer
+      sku       = source_image_reference.value.sku
+      version   = source_image_reference.value.version
   }
 
   os_disk {
@@ -72,10 +54,18 @@ resource "azurerm_linux_virtual_machine" "this" {
       identity_ids = identity.value.identity_ids
     }
   }
+
+  dynamic "additional_capabilities" {
+    for_each = var.vm.additional_capabilities != null ? [var.vm.additional_capabilities] : []
+    content {
+      ultra_ssd_enabled   = additional_capabilities.value.ultra_ssd_enabled
+      hibernation_enabled = additional_capabilities.value.hibernation_enabled
+    }
 }
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface
 resource "azurerm_network_interface" "this" {
+  count                          = var.nic != null ? 1 : 0
   name                           = var.nic.name != null ? var.nic.name : "${var.vm.name}-nic"  # O usa var.nic.name si lo tienes
   location                       = var.common.location
   resource_group_name            = var.common.resource_group_name
@@ -94,8 +84,18 @@ resource "azurerm_network_interface" "this" {
     subnet_id                                          = var.nic.subnet_id != null ? var.nic.subnet_id : data.azurerm_subnet.this[0].id
     private_ip_address_version                         = var.nic.private_ip_address_version
     private_ip_address_allocation                      = var.nic.private_ip_address_allocation
-    public_ip_address_id                               = var.nic.public_ip_address_id
+    public_ip_address_id = (
+      var.nic != null && var.nic.public_ip != null ? data.azurerm_public_ip.this[0].id :
+      var.nic != null ? var.nic.public_ip_address_id :
+      null
+    )
     primary                                            = var.nic.primary
     private_ip_address                                 = var.nic.private_ip_address
   }
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface_security_group_association
+resource "azurerm_network_interface_security_group_association" "this" {
+  count                     = var.nic != null && var.nic.nsg != null ? 1 : 0
+  network_interface_id      = azurerm_network_interface.this[0].id
+  network_security_group_id = data.azurerm_network_security_group.this[0].id
 }
