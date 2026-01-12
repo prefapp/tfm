0cc3b3349cdb1d76361448ed7e5ebb4a51ad4c48
Félix González <felix.gonzalez@prefapp.es>
2026-01-12 16:21:44 +0100

    Fix error obtaining alb lb target group name

--- modules/aws-amq-rabbit/outputs.tf
+++ modules/aws-amq-rabbit/outputs.tf
@@ -24,13 +24,23 @@ output "target_group_arn" {
   value = length(aws_lb_target_group.this) > 0 ? aws_lb_target_group.this[0].arn : null
 }
 
-output "target_group_name" {
-  description = "Name of the Target Group used for the broker."
-  value = length(aws_lb_target_group.this) > 0 ? aws_lb_target_group.this[0].name : null
+output "target_group_arn" {
+  value       = length(aws_lb_target_group.this) > 0 ? values(aws_lb_target_group.this)[0].arn : null
+  description = "ARN of the first Target Group used for the broker. If multiple ports are exposed, this is the first."
 }
 
-output "nlb_dns_name" {
-  description = "Static DNS name provided by the Network Load Balancer"
-  value = length(aws_lb.this) > 0 ? aws_lb.this[0].dns_name : null
+output "target_group_name" {
+  value       = length(aws_lb_target_group.this) > 0 ? values(aws_lb_target_group.this)[0].name : null
+  description = "Name of the first Target Group used for the broker. If multiple ports are exposed, this is the first."
+}
+
+output "target_group_arns" {
+  value       = [for tg in values(aws_lb_target_group.this) : tg.arn]
+  description = "List of all Target Group ARNs used for the broker."
+}
+
+output "target_group_names" {
+  value       = [for tg in values(aws_lb_target_group.this) : tg.name]
+  description = "List of all Target Group names used for the broker."
 }
 
