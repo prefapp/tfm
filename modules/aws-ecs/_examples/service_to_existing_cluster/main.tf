575d324f8ff4aadd165b73529807506d620c38f9
Rubén Castrelo Suarez <kastras@gmail.com>
2026-01-15 10:10:08 +0100

    docs(aws-ecs): add new examples

---
+++ modules/aws-ecs/_examples/service_to_existing_cluster/main.tf
@@ -0,0 +1,63 @@
+# Example: Basic ECS Service with ALB + Fargate
+
+terraform {
+  required_version = ">= 1.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "ecs" {
+  source = "../../"
+
+  cluster_name          = "example-autoscaling-cluster"
+  create_cluster        = false
+  service_name          = "example-basic-service"
+  container_definitions = file("${path.module}/container.json")
+  security_groups = []
+  # security_groups = ["sg-0123456789abcdef0"]
+  existing_alb_name = "ecs-example-autoscaling-cluster"
+  static_content_host_header = "example.mydomain.com"
+  subnet_tag_key  = "Public"
+  subnet_tag_name = "Network"
+  vpc_tag_name    = "beginners-mq-vpc"
+
+  ecs_autoscaling = {
+    basic = {
+      autoscaling_enabled = false
+      min_capacity        = 1
+      max_capacity        = 1
+
+      metric_type      = "CPUUtilization"
+      metric_statistic = "Average"
+
+      scale = {
+        up = {
+          threshold           = 70
+          comparison_operator = "GreaterThanThreshold"
+          evaluation_periods  = 2
+          period              = 60
+          cooldown            = 60
+          adjustment_type     = "ChangeInCapacity"
+          scaling_adjustment  = 1
+        }
+        down = {
+          threshold           = 30
+          comparison_operator = "LessThanThreshold"
+          evaluation_periods  = 2
+          period              = 60
+          cooldown            = 60
+          adjustment_type     = "ChangeInCapacity"
+          scaling_adjustment  = -1
+        }
+      }
+    }
+  }
+}
