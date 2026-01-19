6d97f911da006805a5a7e0cf193fc2395766ace4
Félix González <felix.gonzalez@prefapp.es>
2026-01-19 13:34:09 +0100

    Fix example

--- modules/aws-amq-rabbit/_examples/private_broker_with_nlb/main.tf
+++ modules/aws-amq-rabbit/_examples/private_broker_with_nlb/main.tf
@@ -11,10 +11,16 @@ module "amq_rabbit" {
   }
   lb_certificate_arn = "arn:aws:acm:region:account:certificate/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
   allowed_ingress_cidrs = ["10.0.0.0/8", "192.168.0.0/16"]
-  nlb_listener_ips = {
-    "5671"  = ["10.0.1.10", "10.0.2.10"] # AMQPS
-    "15672" = ["10.0.1.11"]               # Management UI
-  }
+  nlb_listener_ips = [
+    {
+      ips = ["10.0.1.10", "10.0.2.10"]
+      target_port = 5671 # AMQPS
+    },
+    {
+      ips = ["10.0.1.11"]
+      target_port = 15672 # Management UI
+    }
+  ]
   tags = {
     Owner      = "DevOps"
     CostCenter = "IT"
