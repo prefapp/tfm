4731ef4cd8fbfdbbe4dcf660af87ad8b25783068
Félix González <felix.gonzalez@prefapp.es>
2026-02-02 17:14:01 +0100

    Add S3 bucket policy for cloudtrail

--- modules/aws-secretsmanager-replication/main.tf
+++ modules/aws-secretsmanager-replication/main.tf
@@ -97,3 +97,31 @@ resource "aws_cloudtrail" "secrets_management_events" {
   tags = var.tags
 }
 
+resource "aws_s3_bucket_policy" "cloudtrail" {
+  bucket = aws_s3_bucket.cloudtrail.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AWSCloudTrailAclCheck"
+        Effect    = "Allow"
+        Principal = { Service = "cloudtrail.amazonaws.com" }
+        Action    = "s3:GetBucketAcl"
+        Resource  = aws_s3_bucket.cloudtrail.arn
+      },
+      {
+        Sid       = "AWSCloudTrailWrite"
+        Effect    = "Allow"
+        Principal = { Service = "cloudtrail.amazonaws.com" }
+        Action    = "s3:PutObject"
+        Resource  = "${aws_s3_bucket.cloudtrail.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
