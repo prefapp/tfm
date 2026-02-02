439ceb84482370da4c8c5e8fb88b36dae53a6c81
Félix González <felix.gonzalez@prefapp.es>
2026-02-02 09:14:40 +0100

    Add permissions olicy to s3 bucket

--- modules/aws-secretsmanager-replication/cloudtrail.tf
+++ modules/aws-secretsmanager-replication/cloudtrail.tf
@@ -16,6 +16,33 @@ resource "aws_s3_bucket" "cloudtrail_logs" {
   bucket = var.cloudtrail_bucket_name == null ? "${var.prefix}-cloudtrail-logs" : var.cloudtrail_bucket_name
 }
 
+resource "aws_s3_bucket_policy" "cloudtrail_logs" {
+  count  = var.cloudtrail_bucket_name == null ? 1 : 0
+  bucket = aws_s3_bucket.cloudtrail_logs[0].id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = { Service = "cloudtrail.amazonaws.com" }
+        Action = "s3:GetBucketAcl"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.cloudtrail_logs[0].id}"
+      },
+      {
+        Effect = "Allow"
+        Principal = { Service = "cloudtrail.amazonaws.com" }
+        Action = "s3:PutObject"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.cloudtrail_logs[0].id}/AWSLogs/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
+
 locals {
   cloudtrail_bucket = var.cloudtrail_bucket_name != null ? var.cloudtrail_bucket_name : aws_s3_bucket.cloudtrail_logs[0].id
 }
