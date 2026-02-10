7c3056ce1ad4d3eca7b4b4366ddb6659f6f5ecd9
Félix González <felix.gonzalez@prefapp.es>
2026-02-10 09:01:36 +0100

    Add blank lines  to comply with PEP 8

--- modules/aws-secretsmanager-replication/lambda_automatic_replication/src/replication.py
+++ modules/aws-secretsmanager-replication/lambda_automatic_replication/src/replication.py
@@ -114,9 +114,10 @@ def replicate_secret(secret_id: str, config, get_sm_client=None):
                             Tags=source_tags
                         )
 
-            log("info", "Replication completed", account_id=account_id, region=region_name)
-
+    log("info", "Replication completed", account_id=account_id, region=region_name)
     log("info", "Replication finished for all destinations", secret_id=secret_id)
+
+
 def replicate_all(config):
     """
     Replicates all secrets in the source account to all configured destinations and regions.
