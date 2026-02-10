95bb568ec80c779701b89965be52426ef7dea16b
Félix González <felix.gonzalez@prefapp.es>
2026-02-10 13:26:36 +0100

    Add security condition to source account

--- modules/aws-secretsmanager-replication/main.tf
+++ modules/aws-secretsmanager-replication/main.tf
@@ -389,6 +389,11 @@ resource "aws_s3_bucket_policy" "cloudtrail" {
             "s3:GetBucketPolicy"
           ]
           Resource = local.s3_bucket_arn
+          Condition = {
+            StringEquals = {
+              "aws:SourceAccount" = data.aws_caller_identity.current.account_id
+            }
+          }
         },
         {
           Sid       = "AWSCloudTrailWrite"
@@ -399,6 +404,7 @@ resource "aws_s3_bucket_policy" "cloudtrail" {
           Condition = {
             StringEquals = {
               "s3:x-amz-acl" = "bucket-owner-full-control"
+              "aws:SourceAccount" = data.aws_caller_identity.current.account_id
             }
           }
         }
