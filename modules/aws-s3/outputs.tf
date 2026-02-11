fd68c36cce30a83ba31f123811f1bfc420c32fd2
Rubén Castrelo Suarez <kastras@gmail.com>
2026-02-11 11:02:18 +0100

    feat(aws-s3): added description to outputs

--- modules/aws-s3/outputs.tf
+++ modules/aws-s3/outputs.tf
@@ -1,23 +1,29 @@
 output "bucket_id" {
-  value = var.create_bucket ? aws_s3_bucket.this[0].id : data.aws_s3_bucket.this[0].id
+  description = "Name of the bucket that is created or whose data is obtained"
+  value       = var.create_bucket ? aws_s3_bucket.this[0].id : data.aws_s3_bucket.this[0].id
 }
 
 output "bucket_arn" {
-  value = var.create_bucket ? aws_s3_bucket.this[0].arn : data.aws_s3_bucket.this[0].arn
+  description = "ARN of the bucket that is created or whose data is obtained"
+  value       = var.create_bucket ? aws_s3_bucket.this[0].arn : data.aws_s3_bucket.this[0].arn
 }
 output "bucket_domain_name" {
-  value = var.create_bucket ? aws_s3_bucket.this[0].bucket_domain_name : data.aws_s3_bucket.this[0].bucket_domain_name
+  description = "Domain name of the bucket that is created or whose data is obtained"
+  value       = var.create_bucket ? aws_s3_bucket.this[0].bucket_domain_name : data.aws_s3_bucket.this[0].bucket_domain_name
 }
 
 ## Replication outputs
 output "replication_role_arn" {
-  value = try(aws_iam_role.replication[0].arn, null)
+  description = "ARN of the replication role"
+  value       = try(aws_iam_role.replication[0].arn, null)
 }
 
 output "replication_destination_bucket_arn" {
-  value = try(var.s3_replication_destination.bucket_arn, null)
+  description = "ARN of the replication destination bucket"
+  value       = try(var.s3_replication_destination.bucket_arn, null)
 }
 
-output "replication_s3_destination_replication_s3_policydestination_policy" {
-  value = try(data.aws_iam_policy_document.destination_replication_s3_policy[0].json, null)
+output "replication_s3_destination_replication_s3_policy_json" {
+  description = "JSON policy for S3 replication in the destination bucket, to be applied to that bucket."
+  value       = try(data.aws_iam_policy_document.destination_replication_s3_policy[0].json, null)
 }
