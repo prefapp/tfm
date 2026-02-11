61b0cd4535c226be4bc7b35305e29cd3c85a30d9
Félix González <felix.gonzalez@prefapp.es>
2026-02-11 10:45:41 +0100

    Fix indentation and logs

--- modules/aws-secretsmanager-replication/lambda_automatic_replication/src/replication.py
+++ modules/aws-secretsmanager-replication/lambda_automatic_replication/src/replication.py
@@ -84,7 +84,7 @@ def replicate_secret(secret_id: str, config, get_sm_client=None):
                             Tags=source_tags
                         )
 
-        log("info", "Replication completed", account_id=account_id, region=region_name)
+            log("info", "Replication completed for account", account_id=account_id)
 
     log("info", "Replication finished for all destinations", secret_id=secret_id)
 
