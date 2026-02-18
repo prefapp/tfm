578ad4762b663972ef5f15aa0c3fe53235299391
Félix González <felix.gonzalez@prefapp.es>
2026-02-18 09:41:45 +0100

    Fix regex

--- modules/aws-secretsmanager-replication/locals.tf
+++ modules/aws-secretsmanager-replication/locals.tf
@@ -19,7 +19,7 @@ locals {
     length(aws_cloudtrail.secrets_management_events) > 0 ? aws_cloudtrail.secrets_management_events[0].arn : ""
   )
 
-  s3_bucket_id       = local.using_existing_s3_bucket ? regexall("^arn:aws:s3:::(.+)$", var.s3_bucket_arn)[0] : (length(aws_s3_bucket.cloudtrail) > 0 ? aws_s3_bucket.cloudtrail[0].id : null)
+  s3_bucket_id       = local.using_existing_s3_bucket ? try(regexall("^arn:aws:s3:::(.+)$", var.s3_bucket_arn)[0], "INVALID_S3_BUCKET_ARN") : (length(aws_s3_bucket.cloudtrail) > 0 ? aws_s3_bucket.cloudtrail[0].id : null)
   s3_bucket_arn      = local.using_existing_s3_bucket ? var.s3_bucket_arn : (length(aws_s3_bucket.cloudtrail) > 0 ? aws_s3_bucket.cloudtrail[0].arn : null)
   s3_bucket_logs_arn = local.using_existing_s3_bucket ? "${var.s3_bucket_arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*" : (length(aws_s3_bucket.cloudtrail) > 0 ? "${aws_s3_bucket.cloudtrail[0].arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*" : null)
   # has_existing_bucket is only defined once above
