871e7156e1fabc719a68be059fe9280561ad1127
Félix González <felix.gonzalez@prefapp.es>
2026-02-19 17:46:25 +0100

    Add default module options

--- modules/aws-eks/karpenter.tf
+++ modules/aws-eks/karpenter.tf
@@ -5,11 +5,11 @@ module "karpenter" {
   version      = "21.0.0"
   cluster_name = var.cluster_name
 
-  iam_role_name                = format("%s-karpenter-role", var.cluster_name) # Used to generate the instance profile
-  create_iam_role              = true
-  iam_role_use_name_prefix     = false
-  iam_policy_use_name_prefix   = true
-  create_access_entry          = true
+  iam_role_name              = format("%s-karpenter-role", var.cluster_name) # Used to generate the instance profile
+  create_iam_role            = true
+  iam_role_use_name_prefix   = false
+  iam_policy_use_name_prefix = true
+  create_access_entry        = true
   node_iam_role_additional_policies = {
     AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
     Ec2ExtraPolicy               = aws_iam_policy.iam_policy_extra_karpenter[0].arn
