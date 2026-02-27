176c93c07fb6ecd9ee080f98cf2845672cf7df3c
jcframil <framiljuan@gmail.com>
2026-02-27 14:57:35 +0100

    fix: adapt variables
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-vnet-gateway/vpn_nat_rule.tf
+++ modules/azure-vnet-gateway/vpn_nat_rule.tf
@@ -1,5 +1,6 @@
 ## VPN NAT RULE SECTION
 
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network_gateway_nat_rule
 resource "azurerm_virtual_network_gateway_nat_rule" "this" {
   for_each = { for idx, rule in var.nat_rules : idx => rule }
   name                       = each.value.name
@@ -7,7 +8,7 @@ resource "azurerm_virtual_network_gateway_nat_rule" "this" {
   virtual_network_gateway_id = azurerm_virtual_network_gateway.this.id
   mode                       = each.value.mode
   type                       = each.value.type
-  ip_configuration_id        = each.value.ip_configuration_id
+  ip_configuration_id        = azurerm_virtual_network_gateway.this.ip_configuration.id
 
   external_mapping {
     address_space = each.value.external_mapping_address_space
