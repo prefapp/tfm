bd0abd01ff027daafc5e7641fc4bb58d02989704
jcframil <framiljuan@gmail.com>
2026-02-27 13:13:41 +0100

    fix: tags linked to instance
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-localnet-gateway/locals.tf
+++ modules/azure-localnet-gateway/locals.tf
@@ -4,7 +4,7 @@ locals {
   # tags por cada localnet (key = idx)
   tags = { for idx, s in var.localnet :
     idx => (
-      try(s.tags_from_rg, false)
+      coalesce(s.tags_from_rg, false)
       ? merge(lookup(data.azurerm_resource_group.this, idx, null) != null ? data.azurerm_resource_group.this[idx].tags : {}, try(s.tags, {}))
       : try(s.tags, {})
     )
