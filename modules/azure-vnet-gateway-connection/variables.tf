f316eaca50b4a4656c28c3529e6a2f58466421a6
jcframil <framiljuan@gmail.com>
2026-03-06 10:10:20 +0100

    fix: claims vnetgateway, localnetgateway, connection
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-vnet-gateway-connection/variables.tf
+++ modules/azure-vnet-gateway-connection/variables.tf
@@ -3,18 +3,32 @@
 variable "connection" {
   description = "List of Site-to-Site VPN connection objects"
   type = list(object({
-    name                       = string
-    location                   = string
-    resource_group_name        = string
-    local_gateway_name         = string
-    local_gateway_resource_group_name = string
-    type                       = string
-    gateway_name               = string
-    enable_bgp                 = bool
-    shared_key                 = optional(string)
-    keyvault_secret_name       = optional(string)
-    keyvault_vault_name        = optional(string)
-    keyvault_vault_rg          = optional(string)
+    name                                = string
+    location                            = string
+    resource_group_name                 = string
+    local_gateway_name                  = string
+    local_gateway_resource_group_name   = string
+    type                                = string
+    gateway_name                        = string
+      # DEPRECATED: enable_bgp is deprecated in azurerm_virtual_network_gateway_connection
+      enable_bgp                          = optional(bool)
+    shared_key                          = optional(string)
+    keyvault_secret_name                = optional(string)
+    keyvault_vault_name                 = optional(string)
+    keyvault_vault_rg                   = optional(string)
+    virtual_network_gateway_id          = optional(string)
+    local_network_gateway_id            = optional(string)
+    connection_protocol                 = optional(string)
+    routing_weight                      = optional(number)
+    authorization_key                   = optional(string)
+    express_route_circuit_id            = optional(string)
+    peer_virtual_network_gateway_id     = optional(string)
+    use_policy_based_traffic_selectors  = optional(bool)
+    express_route_gateway_bypass        = optional(bool)
+    dpd_timeout_seconds                 = optional(number)
+    connection_mode                     = optional(string)
+    tags_from_rg                        = optional(bool)
+    tags                                = optional(map(string))
     ipsec_policy = optional(object({
       dh_group         = string
       ike_encryption   = string
@@ -24,9 +38,7 @@ variable "connection" {
       pfs_group        = string
       sa_lifetime      = number
       sa_datasize      = number
-    }) )
-    tags_from_rg               = optional(bool)
-    tags                       = optional(map(string))
+    }))
   }))
   default = []
 }
