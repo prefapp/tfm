76954c43ba59b266f37f127fec325bcbee9415b2
frmadem <frmadem@hotmail.com>
2026-03-07 19:04:23 +0100

    Add versions

--- modules/gh-repo-secrets-section/variables.tf
+++ modules/gh-repo-secrets-section/variables.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 6.0"
-    }
-  }
-}
-
 variable "config" {
   description = <<EOT
 GitHub repository secrets configuration as a single complex object.
