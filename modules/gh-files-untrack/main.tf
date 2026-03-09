05c77f96381abe7aa8a6922bfd06af1914828528
frmadem <frmadem@hotmail.com>
2026-03-09 13:38:55 +0100

    WIP

--- modules/gh-files-untrack/main.tf
+++ modules/gh-files-untrack/main.tf
@@ -1,18 +1,20 @@
 resource "null_resource" "untrack" {
   for_each = {
     for f in var.config.files :
-    "${f.repository}/${f.file}/${f.branch}" => f
+    "${f.repository}/${f.file}/${coalesce(f.branch, "main")}" => f
   }
 
+  # We store the exact Terraform resource address during creation
+  # so it can be safely referenced during destroy
   triggers = {
-    full_address = "github_repository_file.user_managed[\"${each.value.repository}/${each.value.file}/${each.value.branch}\"]"
+    address = "github_repository_file.user_managed[\"${each.value.repository}/${each.value.file}/${coalesce(each.value.branch, "main")}\"]"
   }
 
   provisioner "local-exec" {
     when    = destroy
     command = <<-EOT
       echo "🔓 Untracking user-managed file: ${each.value.repository}/${each.value.file}"
-      terraform state rm '${self.triggers.full_address}' || true
+      terraform state rm '${self.triggers.address}' || true
     EOT
   }
 }
