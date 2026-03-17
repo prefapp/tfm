c7d4e62ce43ac4b94f237c6877e5b8a9fc08a139
jcframil <framiljuan@gmail.com>
2026-03-17 11:01:16 +0100

    fix: copilot suggestions
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-vnet-gateway/vpn_nat_rule.tf
+++ modules/azure-vnet-gateway/vpn_nat_rule.tf
@@ -5,17 +5,17 @@ resource "azurerm_virtual_network_gateway_nat_rule" "this" {
   for_each                   = { for rule in var.nat_rules : rule.name => rule }
   name                       = each.value.name
   resource_group_name        = var.vpn.resource_group_name
-  virtual_network_gateway_id = azurerm_virtual_network_gateway.this.id
+  virtual_network_gateway_id = azurerm_virtual_network_gateway.this[0].id
   mode                       = each.value.mode
   type                       = each.value.type
-  ip_configuration_id = coalesce(
-    try(each.value.ip_configuration_id, null),
-    try(data.azurerm_virtual_network_gateway.this[0].ip_configuration[0].id, null)
+
+  ip_configuration_id = try(
+    each.value.ip_configuration_id,
+    azurerm_virtual_network_gateway.this[0].ip_configuration[0].id
   )
 
   dynamic "external_mapping" {
     for_each = each.value.external_mapping
-
     content {
       address_space = external_mapping.value.address_space
       port_range    = external_mapping.value.port_range
@@ -24,10 +24,19 @@ resource "azurerm_virtual_network_gateway_nat_rule" "this" {
 
   dynamic "internal_mapping" {
     for_each = each.value.internal_mapping
-
     content {
       address_space = internal_mapping.value.address_space
       port_range    = internal_mapping.value.port_range
+    }
+  }
+
+  lifecycle {
+    precondition {
+      condition = (
+        try(each.value.ip_configuration_id, null) != null ||
+        length(azurerm_virtual_network_gateway.this[0].ip_configuration) > 0
+      )
+      error_message = "ip_configuration_id must be provided or derivable from the gateway"
     }
   }
 }
