1b96a9e349aee9f07408cb6c92da9cc48ad06cb6
frmadem <frmadem@hotmail.com>
2026-03-21 01:04:09 +0100

    Fix outputs

--- modules/gh-files-set/outputs.tf
+++ modules/gh-files-set/outputs.tf
@@ -1,4 +1,4 @@
 output "user_managed_files" {
   description = "Files marked as userManaged (will survive destroy)"
-  value       = [for f in var.config.files : "${f.repository}/${f.file}" if f.userManaged]
+  value       = [for f in var.config.files : "${f.file}/${f.branch}" if f.userManaged]
 }
