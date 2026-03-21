d63731ba7f35b2f9f9597315c4a779469e29da9c
frmadem <frmadem@hotmail.com>
2026-03-21 01:01:55 +0100

    Fix

--- modules/gh-files-set/main.tf
+++ modules/gh-files-set/main.tf
@@ -8,7 +8,7 @@ data "github_repository" "this" {
 # Normal files — Terraform fully enforces content
 resource "github_repository_file" "managed" {
   for_each = {
-    for f in var.config.files : "${f.repository}/${f.file}/${f.branch}" => f
+    for f in var.config.files : "${f.file}/${f.branch}" => f
     if !f.userManaged
   }
 
@@ -23,7 +23,7 @@ resource "github_repository_file" "managed" {
 # User-managed files — provision once + ignore content drift
 resource "github_repository_file" "user_managed" {
   for_each = {
-    for f in var.config.files : "${f.repository}/${f.file}/${f.branch}" => f
+    for f in var.config.files : "${f.file}/${f.branch}" => f
     if f.userManaged
   }
 
