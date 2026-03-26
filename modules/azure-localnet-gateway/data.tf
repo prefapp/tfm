2b6a9e8656986bcf191822b65c5844186aad7671
jcframil <framiljuan@gmail.com>
2026-03-26 22:28:49 +0100

    fix: localnet data
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-localnet-gateway/data.tf
+++ modules/azure-localnet-gateway/data.tf
@@ -2,6 +2,6 @@
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/resource_group
 data "azurerm_resource_group" "this" {
-  for_each = { for idx, s in var.localnet : idx => s if try(s.tags_from_rg, false) }
+  for_each = { for idx, s in var.localnet : idx => s if coalesce(s.tags_from_rg, false) }
   name     = each.value.resource_group_name
 }
