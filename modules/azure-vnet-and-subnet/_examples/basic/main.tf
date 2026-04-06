5d4ac7428be35fee6da89c6dc553a508cf8e737d
pablo.sanchez <pablo.sanchez@prefapp.es>
2026-04-06 13:07:34 +0200

    docs(azure-vnet-and-subnet-docs): update branch and fix errors

--- modules/azure-vnet-and-subnet/_examples/basic/main.tf
+++ modules/azure-vnet-and-subnet/_examples/basic/main.tf
@@ -1,25 +1,13 @@
 // Basic example: Virtual Network with one subnet using azure-vnet-and-subnet module
 
+locals {
+  values = yamldecode(file("./values.yaml"))
+}
+
 module "azure_vnet_and_subnet" {
   source = "../../"
 
-  resource_group_name = "example-rg"
-
-  virtual_network = {
-    name          = "example-vnet"
-    location      = "westeurope"
-    address_space = ["10.0.0.0/16"]
-    subnets = {
-      internal = {
-        address_prefixes = ["10.0.1.0/24"]
-      }
-    }
-  }
-
-  private_dns_zones = []
-  peerings          = []
-
-  tags = {
-    environment = "dev"
-  }
+  virtual_network   = local.values.virtual_network
+  private_dns_zones = local.values.private_dns_zones
+  peerings          = local.values.peerings
 }
