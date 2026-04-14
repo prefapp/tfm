c6719b70d5a92b592b331149271811417915165e
Juan Ramón Sixto Anello <juan@sixtoanello.com>
2026-04-15 00:11:40 +0200

    fix(github-org-rulesets): first implementation

--- modules/github-org-rulesets/outputs.tf
+++ modules/github-org-rulesets/outputs.tf
@@ -1,6 +1,6 @@
 output "ruleset_ids" {
-  description = "Map of ruleset logical key to ruleset GraphQL node_id"
-  value       = { for k, v in github_organization_ruleset.this : k => v.node_id }
+  description = "Map of ruleset logical key to ruleset GraphQL ruleset_id"
+  value       = { for k, v in github_organization_ruleset.this : k => v.ruleset_id }
 }
 
 output "node_ids" {
