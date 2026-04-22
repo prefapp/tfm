a0ca9eac215b71b0ce979847152adb361a05f3cf
pablo.sanchez <pablo.sanchez@prefapp.es>
2026-04-22 11:07:39 +0200

    docs(azure-customrole-docs) add outputs

--- modules/azure-customrole/outputs.tf
+++ modules/azure-customrole/outputs.tf
@@ -2,3 +2,13 @@ output "id" {
   description = "GUID of the custom role definition (`role_definition_id` from the created resource)."
   value       = azurerm_role_definition.this.role_definition_id
 }
+
+output "role_definition_id" {
+  description = "The Role Definition ID."
+  value       = azurerm_role_definition.this.role_definition_id
+}
+
+output "role_definition_resource_id" {
+  description = "The Azure Resource Manager ID for the resource."
+  value       = azurerm_role_definition.this.role_definition_resource_id
+}
