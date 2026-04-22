5b17ff3449aa3ffd03fe8e54146ee6ca05f613ee
pablo.sanchez <pablo.sanchez@prefapp.es>
2026-04-22 15:39:11 +0200

    docs(azure-nsg-nsr-docs) add outputs

--- modules/azure-nsg-nsr/outputs.tf
+++ modules/azure-nsg-nsr/outputs.tf
@@ -2,3 +2,8 @@ output "id" {
   description = "Resource ID of the network security group."
   value       = azurerm_network_security_group.this.id
 }
+
+output "network_security_rule_id" {
+  description = "Map of rule keys (from `rules`) to network security rule resource IDs."
+  value       = { for k, v in azurerm_network_security_rule.this : k => v.id }
+}
