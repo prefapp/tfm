0338526f7c3a559322c35bf5b03340309c5e7196
pablo.sanchez <pablo.sanchez@prefapp.es>
2026-04-22 15:32:14 +0200

    docs(azure-mi-docs) add outputs

--- modules/azure-mi/outputs.tf
+++ modules/azure-mi/outputs.tf
@@ -2,3 +2,13 @@ output "id" {
   description = "Resource ID of the user-assigned managed identity."
   value       = azurerm_user_assigned_identity.this.id
 }
+
+output "principal_id" {
+  description = "Principal (object) ID of the identity; use for Azure RBAC `principal_id`."
+  value       = azurerm_user_assigned_identity.this.principal_id
+}
+
+output "client_id" {
+  description = "Client (application) ID of the identity; use when the workload must select this UAMI explicitly."
+  value       = azurerm_user_assigned_identity.this.client_id
+}
