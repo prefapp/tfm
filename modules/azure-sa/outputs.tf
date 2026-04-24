87a848b083a9f8cd4b5178a1e8e02701749de02d
JorgeIglesiasMosquera <jimgotch@gmail.com>
2026-04-24 15:03:51 +0200

    doc(azure-sa): modify outputs

--- modules/azure-sa/outputs.tf
+++ modules/azure-sa/outputs.tf
@@ -1,16 +1,53 @@
-output "id" {
+output "storage_account_id" {
   description = "Resource ID of the storage account."
   value       = azurerm_storage_account.this.id
 }
 
-output "name" {
+output "storage_account_name" {
   description = "Name of the storage account."
   value       = azurerm_storage_account.this.name
+}
+
+output "primary_access_key" {
+  description = "Primary access key for the storage account."
+  value       = azurerm_storage_account.this.primary_access_key
+  sensitive   = true
+}
+
+output "primary_connection_string" {
+  description = "Primary connection string for the storage account."
+  value       = azurerm_storage_account.this.primary_connection_string
+  sensitive   = true
 }
 
 output "primary_blob_endpoint" {
   description = "Primary blob service endpoint URL."
   value       = azurerm_storage_account.this.primary_blob_endpoint
+}
+
+output "primary_endpoints" {
+  description = "Primary endpoints for the storage account."
+  value = {
+    blob  = azurerm_storage_account.this.primary_blob_endpoint
+    queue = azurerm_storage_account.this.primary_queue_endpoint
+    table = azurerm_storage_account.this.primary_table_endpoint
+    file  = azurerm_storage_account.this.primary_file_endpoint
+  }
+}
+
+output "secondary_blob_endpoint" {
+  description = "Secondary blob service endpoint URL."
+  value       = azurerm_storage_account.this.secondary_blob_endpoint
+}
+
+output "secondary_endpoints" {
+  description = "Secondary endpoints for the storage account."
+  value = {
+    blob  = azurerm_storage_account.this.secondary_blob_endpoint
+    queue = azurerm_storage_account.this.secondary_queue_endpoint
+    table = azurerm_storage_account.this.secondary_table_endpoint
+    file  = azurerm_storage_account.this.secondary_file_endpoint
+  }
 }
 
 output "container_ids" {
