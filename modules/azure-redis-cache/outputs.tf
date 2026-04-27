b3626cc9c3e0ed0f8d99aeec956768ac5a56680c
JorgeIglesiasMosquera <jimgotch@gmail.com>
2026-04-27 18:21:03 +0200

    doc(azure-redis-cache): add outputs

--- modules/azure-redis-cache/outputs.tf
+++ modules/azure-redis-cache/outputs.tf
@@ -13,7 +13,38 @@ output "ssl_port" {
   value       = azurerm_redis_cache.this.ssl_port
 }
 
+output "port" {
+  description = "Non-SSL port of the Redis cache."
+  value       = azurerm_redis_cache.this.port
+}
+
+output "redis_connection" {
+  description = "Connection information for Redis."
+  value = {
+    hostname = azurerm_redis_cache.this.hostname
+    ssl_port = azurerm_redis_cache.this.ssl_port
+    port     = azurerm_redis_cache.this.port
+  }
+}
+
+output "primary_access_key" {
+  description = "Primary access key for the Redis cache."
+  value       = azurerm_redis_cache.this.primary_access_key
+  sensitive   = true
+}
+
+output "secondary_access_key" {
+  description = "Secondary access key for the Redis cache."
+  value       = azurerm_redis_cache.this.secondary_access_key
+  sensitive   = true
+}
+
 output "private_endpoint_id" {
   description = "Resource ID of the private endpoint for the Redis cache."
   value       = azurerm_private_endpoint.this.id
+}
+
+output "private_endpoint_private_ip" {
+  description = "Private IP address of the private endpoint."
+  value       = azurerm_private_endpoint.this.private_service_connection[0].private_ip_address
 }
