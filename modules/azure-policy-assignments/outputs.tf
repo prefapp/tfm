7d78a5f77a3ec13a0ffe78916bb3f5c587463980
pablo.sanchez <pablo.sanchez@prefapp.es>
2026-04-28 10:46:47 +0200

    docs(azure-policy-assignments-docs) update docs

--- modules/azure-policy-assignments/outputs.tf
+++ modules/azure-policy-assignments/outputs.tf
@@ -3,9 +3,25 @@ output "resource_policy_assignment_ids" {
   value       = [for assignment in azurerm_resource_policy_assignment.this : assignment.id]
 }
 
+output "resource_policy_assignments" {
+  description = "List of objects for each Azure resource policy assignment, including id and identity principal_id."
+  value = [for assignment in azurerm_resource_policy_assignment.this : {
+    id           = assignment.id
+    principal_id = try(assignment.identity[0].principal_id, null)
+  }]
+}
+
 output "resource_group_policy_assignment_ids" {
   description = "List of all Azure resource group policy assignment IDs"
   value       = [for assignment in azurerm_resource_group_policy_assignment.this : assignment.id]
+}
+
+output "resource_group_policy_assignments" {
+  description = "List of objects for each Azure resource group policy assignment, including id and identity principal_id."
+  value = [for assignment in azurerm_resource_group_policy_assignment.this : {
+    id           = assignment.id
+    principal_id = try(assignment.identity[0].principal_id, null)
+  }]
 }
 
 output "subscription_policy_assignment_ids" {
@@ -13,7 +29,23 @@ output "subscription_policy_assignment_ids" {
   value       = [for assignment in azurerm_subscription_policy_assignment.this : assignment.id]
 }
 
+output "subscription_policy_assignments" {
+  description = "List of objects for each Azure subscription policy assignment, including id and identity principal_id."
+  value = [for assignment in azurerm_subscription_policy_assignment.this : {
+    id           = assignment.id
+    principal_id = try(assignment.identity[0].principal_id, null)
+  }]
+}
+
 output "management_group_policy_assignment_ids" {
   description = "List of all Azure management group policy assignment IDs"
   value       = [for assignment in azurerm_management_group_policy_assignment.this : assignment.id]
+}
+
+output "management_group_policy_assignments" {
+  description = "List of objects for each Azure management group policy assignment, including id and identity principal_id."
+  value = [for assignment in azurerm_management_group_policy_assignment.this : {
+    id           = assignment.id
+    principal_id = try(assignment.identity[0].principal_id, null)
+  }]
 }
