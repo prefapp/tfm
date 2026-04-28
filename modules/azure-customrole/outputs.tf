adb2f774e7e78394b08e3c41e0344be9fd477569
pablosanchezpaz <76182461+pablosanchezpaz@users.noreply.github.com>
2026-04-28 15:26:25 +0200

    Update outputs.tf

--- modules/azure-customrole/outputs.tf
+++ modules/azure-customrole/outputs.tf
@@ -3,11 +3,6 @@ output "id" {
   value       = azurerm_role_definition.this.role_definition_id
 }
 
-output "role_definition_id" {
-  description = "The Role Definition ID."
-  value       = azurerm_role_definition.this.role_definition_id
-}
-
 output "role_definition_resource_id" {
   description = "The Azure Resource Manager ID for the resource."
   value       = azurerm_role_definition.this.role_definition_resource_id
