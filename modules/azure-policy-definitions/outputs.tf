ce07759eaeda6c556f3e0fa69c222d35152dd25a
JorgeIglesiasMosquera <jimgotch@gmail.com>
2026-04-28 13:26:26 +0200

    doc(azure-policy-definitions): add outputs

--- modules/azure-policy-definitions/outputs.tf
+++ modules/azure-policy-definitions/outputs.tf
@@ -7,3 +7,11 @@ output "policy_definition_names" {
   description = "Names of created policy definitions, in lexicographic order of `name`."
   value       = [for policy in azurerm_policy_definition.this : policy.name]
 }
+
+output "policy_definition_ids_map" {
+  description = "Map from policy name to policy definition ID."
+  value = {
+    for k, p in azurerm_policy_definition.this :
+    k => p.id
+  }
+}
