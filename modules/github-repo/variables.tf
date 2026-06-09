45036f81a08387541026933ad0e2c0833f818d18
ricardosacristan <258636693+ricardosacristan@users.noreply.github.com>
2026-06-09 18:33:24 +0200

    remove hasDiscussions

--- modules/github-repo/variables.tf
+++ modules/github-repo/variables.tf
@@ -15,7 +15,6 @@ variable "config" {
       deleteBranchOnMerge = optional(bool, false)
       allowUpdateBranch   = optional(bool, false)
       hasIssues           = optional(bool, true)
-      hasDiscussions      = optional(bool, false)
       hasWiki             = optional(bool, true)
     })
 
