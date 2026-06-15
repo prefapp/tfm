5006c83343ff66315f60b0f4fabaad8b8b71ddf5
Rubén Castrelo Suarez <kastras@gmail.com>
2026-06-15 16:20:40 +0200

    fix(aws-s3): update issue with vars

--- modules/aws-s3/local.tf
+++ modules/aws-s3/local.tf
@@ -1,3 +1,8 @@
 locals {
+  # Validate that if s3_replication_destination is configured, versioning must be enabled
+  validate_replication_versioning = (
+    var.s3_replication_destination == null || var.s3_bucket_versioning == "Enabled"
+  ) ? true : file("ERROR: When configuring s3_replication_destination, s3_bucket_versioning must be set to \"Enabled\".")
+
   lifecycle_rules = (var.s3_replication_destination != null || var.s3_bucket_versioning == "Enabled") ? concat(var.lifecycle_rules, var.default_lifecycle_rules) : var.lifecycle_rules
 }
