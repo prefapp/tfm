19865d78969ee897205768d8897ad00424bb2e75
Félix González <felix.gonzalez@prefapp.es>
2026-06-19 12:45:51 +0200

    feat(aws-parameter-store-replication): remove unused local

--- modules/aws-parameter-store-replication/locals.tf
+++ modules/aws-parameter-store-replication/locals.tf
@@ -13,19 +13,6 @@ locals {
   lambda_manual_policy_name      = "${var.prefix}-${var.name}-replication-manual-policy"
   eventbridge_rule_name          = "${var.prefix}-${var.name}-parameter-changes"
 
-  # Flatten destinations into a list for easier iteration
-  all_account_regions = flatten([
-    for account_id, account_config in local.destinations : [
-      for region, region_config in account_config.regions : {
-        account_id  = account_id
-        region      = region
-        role_arn    = account_config.role_arn
-        kms_key_arn = try(region_config.kms_key_arn, null)
-        kms_key_id  = try(region_config.kms_key_arn, null) != null ? split("/", region_config.kms_key_arn)[length(split("/", region_config.kms_key_arn)) - 1] : null
-      }
-    ]
-  ])
-
   # Tags for all resources
   common_tags = merge(
     var.tags,
