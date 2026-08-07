413292b52636baea714b59813cb1ae41fc3315f8
Félix González <felix.gonzalez@prefapp.es>
2026-08-07 09:49:30 +0200

    fix(aws-eks): normalize ECO_DR scalar inputs

--- modules/aws-eks/variables.tf
+++ modules/aws-eks/variables.tf
@@ -279,7 +279,7 @@ variable "ECO_DR" {
   default     = false
 
   validation {
-    condition     = contains(["true", "false", "1", "0", "on", "off", "yes", "no"], lower(tostring(coalesce(var.ECO_DR, false))))
+    condition     = can(var.ECO_DR == null ? "false" : tostring(var.ECO_DR)) && contains(["true", "false", "1", "0", "on", "off", "yes", "no"], lower(var.ECO_DR == null ? "false" : tostring(var.ECO_DR)))
     error_message = "ECO_DR must be one of: true, false, 1, 0, ON, OFF, yes, no."
   }
 }
