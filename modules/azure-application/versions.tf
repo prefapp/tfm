cd611c6ab179f1d01e44a555871b997c7f8bd94d
Juan CFF <97174951+jcframil@users.noreply.github.com>
2026-08-20 15:05:13 +0200

    fix: add constraint version for azurerm 5.0 (#1387)
    Signed-off-by: jcframil <framiljuan@gmail.com>

--- modules/azure-application/versions.tf
+++ modules/azure-application/versions.tf
@@ -4,17 +4,17 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.16.0"
+      version = "~> 4.16.0, < 5.0.0"
     }
 
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 3.3.0"
+      version = "~> 3.3.0, < 4.0.0"
     }
 
     time = {
       source  = "hashicorp/time"
-      version = "~> 0.13.0"
+      version = "~> 0.13.0, < 1.0.0"
     }
   }
 }
