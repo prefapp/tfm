0a3bc85dfd534bae3196142a4abe35705dd0325e
JulianGV21 <llulian.gomez@gmail.com>
2026-09-08 12:53:40 +0200

    add dns-prefix config

--- modules/azure-aks/aks.tf
+++ modules/azure-aks/aks.tf
@@ -1,7 +1,7 @@
 # AKS section
 module "aks" {
   # https://registry.terraform.io/modules/Azure/avm-res-containerservice-managedcluster/azurerm/latest
-  source = "github.com/Azure/terraform-azurerm-avm-res-containerservice-managedcluster?ref=v0.7.1"
+  source = "github.com/Azure/terraform-azurerm-avm-res-containerservice-managedcluster?ref=v0.8.3"
 
   location                                             = var.location
 
@@ -41,7 +41,8 @@ module "aks" {
     }
   }
 
-  name                                                 = var.aks_prefix
+  name       = "${var.aks_prefix}-aks"
+  dns_prefix = var.aks_prefix
 
   aad_profile = {
     managed           = true
