282e854dd47a47c1cfb27fc8ed38aeba22eebf6f
jcframil <framiljuan@gmail.com>
2026-09-11 14:02:00 +0200

    fix(azure-redis-cache): remove ambiguous state migration

--- modules/azure-redis-cache/private-endpoint.tf
+++ modules/azure-redis-cache/private-endpoint.tf
@@ -1,10 +1,13 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/1.43.0/docs/resources/private_endpoint
 moved {
+<<<<<<< HEAD
   from = azurerm_private_endpoint.private_endpoint
   to   = azurerm_private_endpoint.this["default"]
 }
 
 moved {
+=======
+>>>>>>> 3ac3798 (fix(azure-redis-cache): remove ambiguous state migration)
   from = azurerm_private_endpoint.this
   to   = azurerm_private_endpoint.this["default"]
 }
