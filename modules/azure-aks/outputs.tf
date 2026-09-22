614ab9f10a8ff884888c7a050ba8537ae3c11a69
JulianGV21 <llulian.gomez@gmail.com>
2026-09-22 07:39:05 +0200

    fix output and nodepool

--- modules/azure-aks/outputs.tf
+++ modules/azure-aks/outputs.tf
@@ -31,12 +31,12 @@ output "cluster_issuer" {
 
 # The kubelet identity client ID of the AKS cluster.
 output "kubelet_identity_client_id" {
-  value = module.aks.kubelet_identity[0].client_id
+  value = module.aks.kubelet_identity.clientId
 }
 
 # The kubelet identity object ID of the AKS cluster.
 output "kubelet_identity_object_id" {
-  value = module.aks.kubelet_identity[0].object_id
+  value = module.aks.kubelet_identity.objectId
 }
 
 # The network profile of the AKS cluster. See README for structure.
